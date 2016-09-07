0e9a41cb59ac67f827f973b4f8e67c566ce5f456
Andrew Tritt <ajtritt@lbl.gov>
2016-09-06 17:03:02 -0700

    added writing of schema to a location visibile within the pynwb package to setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from urllib.request import urlopen
+import json
+import pickle
 
+
+def get_schema():
+    '''Here, we will do something to access a URL stored in the pynwb repo.
+    This URL will contain the source of the schema.  
+    '''
+    # we should look this up in a config file somewhere
+    url = "https://bitbucket.org/lblneuro/nwb-schema/downloads/nwb_1.0.4_beta.json"
+    schema = urlopen(url)
+    return schema
 
 with open('README.rst') as f:
     readme = f.read()
@@ -9,7 +21,10 @@ with open('README.rst') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(
+    
+schema_path = 'schema/schema.pkl'
+
+setup_args = {
     name='PyNWB',
     version='0.0.1',
     description='Package for working with Neurodata stored in the NWB format',
@@ -21,13 +36,21 @@ setup(
     #packages=find_packages(exclude=('tests', 'docs'))
     packages=['pynwb'],
     package_dir={'pynwb':'src/pynwb'},
-    data_files=[('schema', ['schema/*.json'])]
-)
+    package_data={'pynwb':[schema_path]}
+}
 
-'''
-Do some stuff here to retrieve (download from centralized location, or parse
-from a location in the repo directory) the schema. Once retrieved write to 
-to Python code somewhere. This could be pickling it, or writing it to a 
-Python file as an ecoded string. This way, modifying the schema will require
-rebuilding the package, and the schema will be hardcoded.
-'''
+if __name__ == '__main__':
+    '''
+    Do some stuff here to retrieve (download from centralized location, or parse
+    from a location in the repo directory) the schema. Once retrieved write to 
+    to Python code somewhere. This could be pickling it, or writing it to a 
+    Python file as an ecoded string. This way, modifying the schema will require
+    rebuilding the package, and the schema will be hardcoded.
+    '''
+    schema = get_schema()
+    with open(schema_path,'w') as schema_out:
+        pickle.dump(schema, schema_out)
+
+    setup(**setup_args)
+
+
