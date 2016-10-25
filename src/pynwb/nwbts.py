7d21637f809d811117035df584f42f0645ca3e98
Andrew Tritt <ajtritt@lbl.gov>
2016-10-24 20:33:34 -0700

    added some placeholders and commented out code in nwbts.py

--- src/pynwb/nwbts.py
+++ src/pynwb/nwbts.py
@@ -38,10 +38,10 @@ import traceback
 import copy
 from . import nwbmo
 
-from . import container
+from .container import Container
 
 
-class TimeSeries(container.Container):
+class TimeSeries(Container):
     """ Standard TimeSeries constructor
 
         All time series are created by calls to  NWB.create_timeseries(). 
@@ -243,7 +243,7 @@ class TimeSeries(container.Container):
            in the sibling time series
    
            Arguments:
-               *sibling* (text) Full HDF5 path to TimeSeries containing source data[] array, or a python TimeSeries object
+               *ts* (text) Full HDF5 path to TimeSeries containing source data[] array, or a python TimeSeries object
    
            Returns:
                *nothing*
@@ -420,193 +420,193 @@ class TimeSeries(container.Container):
     ####################################################################
     # file writing and path management
 
-    @deprecated
-    def set_path(self, path):
-        """ Sets the path for where the *TimeSeries* is created. This
-            is only necessary for *TimeSeries* that were not created
-            indicating the common storage location (ie, acquisition,
-            stimulus, template) or that were not added to a module
-        """
-        if self.finalized:
-            self.fatal_error("Added value after finalization")
-        if path.endswith('/'):
-            self.path = path
-        else:
-            self.path = path + "/"
-        full_path = self.path + self.name
-        if full_path in self.nwb.file_pointer:
-            self.fatal_error("group '%s' already exists" % full_path)
-
-    @deprecated
-    def full_path(self):
-        """ Returns the HDF5 path to this *TimeSeries*
-
-            Arguments: 
-                *none*
-
-            Returns:
-                *nothing*
-        """
-        return self.path + self.name
-
-    @deprecated
-    def finalize(self):
-        """ Finish the *TimSeries* and write pending changes to disk
-
-            Arguments:
-                *none*
-
-            Returns:
-                *nothing*
-        """
-        if self.finalized:
-            return
-        # verify path is in acceptable location
-        # AJTRITT: BEGIN validation
-        valid_loc = False   # ever the pessimest
-        template = False    
-        if self.path.startswith("/acquisition/timeseries"):
-            valid_loc = True
-        elif self.path.startswith("acquisition/timeseries"):
-            valid_loc = True
-        elif self.path.startswith("/stimulus/templates"):
-            valid_loc = True
-            template = True
-        elif self.path.startswith("stimulus/templates"):
-            valid_loc = True
-            template = True
-        elif self.path.startswith("/stimulus/presentation"):
-            valid_loc = True
-        elif self.path.startswith("stimulus/presentation"):
-            valid_loc = True
-        elif self.path.startswith("/analysis"):
-            valid_loc = True
-        elif self.path.startswith("analysis"):
-            valid_loc = True
-        elif self.path.startswith("/processing") and len(self.path) > len("/processing/"):
-            valid_loc = True
-        elif self.path.startswith("processing") and len(self.path) > len("processing/"):
-            valid_loc = True
-        if not valid_loc:
-            print("Timeseries '%s' is not stored in a correct location" % self.name)
-            print("Specified path is '%s'" % self.path)
-            print("Possible solutions:")
-            print("1) when creating the time series, specify modality='acquisition', ")
-            print("   'stimulus' or 'template' and the path will be correctly")
-            print("   assigned")
-            print("2) add the time series to a processing module")
-            print("3) manually assign a valid directory in '/acquisition/',")
-            print("   'stimulus', 'processing' or 'analysis' folders")
-            self.nwb.fatal_error("Invalid TimeSeries path")
-        # AJTRITT: END validation
-
-        valid_loc = False   # ever the pessimest
-        from . import nwb as nwblib
-        nwblib.register_finalization(self.path + self.name, self.serial_num)
-        # tell kernel about link so table of all links can be added to
-        #   file at end
-        if self.data_tgt_path_soft is not None:
-            self.nwb.record_timeseries_data_soft_link(self.full_path(), self.data_tgt_path_soft)
-        if self.data_tgt_path is not None:
-            self.nwb.record_timeseries_data_link(self.full_path(), self.data_tgt_path)
-        if self.time_tgt_path is not None:
-            self.nwb.record_timeseries_time_link(self.full_path(), self.time_tgt_path)
-        # verify all mandatory fields are present
-        # AJTRITT: BEGIN validation
-        spec = self.spec
-        if template:    # VALIDATOR
-            spec["timestamps"]["_include"] = "optional"
-            spec["starting_time"]["_include"] = "optional"
-        # num_samples can sometimes be calculated automatically. do so
-        #   here if that's possible
-        if "_value" not in spec["num_samples"]:
-            if "_value" in spec["timestamps"]:
-                # make tmp short name to avoid passing 80-col limit in editor
-                tdat = spec["timestamps"] 
-                spec["num_samples"]["_value"] = len(tdat["_value"])
-        # document missing standard fields
-        err_str = []
-        missing_fields = []
-        for k in list(spec.keys()):
-            if k.startswith('_'):   # check for leading underscore
-                continue    # control field -- ignore
-            if "_value" in spec[k]:
-                continue    # field exists
-            if spec[k]["_include"] == "required":
-                # value is missing -- see if alternate or link exists
-                if "_value_softlink" in spec[k]:
-                    continue
-                if "_value_hardlink" in spec[k]:
-                    continue
-                # valid alternative fields include links -- check possibilities
-                if "_alternative" in spec[k]:
-                    if "_value" in spec[spec[k]["_alternative"]]:
-                        continue    # alternative field exists
-                    if "_value_hardlink" in spec[spec[k]["_alternative"]]:
-                        continue    # alternative field exists
-                    if "_value_softlink" in spec[spec[k]["_alternative"]]:
-                        continue    # alternative field exists
-                miss_str = "Missing field '%s'" % k
-                if "_alternative" in spec[k]:
-                    miss_str += " (or '%s')" % spec[k]["_alternative"]
-                err_str.append(str(miss_str))
-            # make a record of missing required fields
-            if spec[k]["_include"] == "standard":
-                if "_value" not in spec["_attributes"]["missing_fields"]:
-                    spec["_attributes"]["missing_fields"]["_value"] = []
-                spec["_attributes"]["missing_fields"]["_value"].append(str(k))
-                missing_fields.append(str(k))
-        # add spec's _description to 'help' field
-        # use asserts -- there's a problem w/ the spec if these don't exist
-        assert "help" in spec["_attributes"]
-        assert "_description" in spec
-        spec["_attributes"]["help"]["_value"] = spec["_description"]
-        # make sure that mandatory attributes are present
-        lspec = []
-        lspec.append(spec)
-        if "_value" in spec["data"]:
-            lspec.append(spec["data"])
-        if "_value" in spec["timestamps"]:
-            lspec.append(spec["timestamps"])
-        if "_value" in spec["starting_time"]:
-            lspec.append(spec["starting_time"])
-        for i in range(len(lspec)):
-            for k in lspec[i]["_attributes"]:
-                if lspec[i]["_attributes"][k]["_include"] == "required":
-                    if "_value" not in lspec[i]["_attributes"][k]:
-                        err_str.append("Missing attribute: " + k)
-        # AJTRITT: END validation
-
-        # report missing standard data
-        if len(missing_fields) > 0:
-            missing_fields.sort()
-            spec["_attributes"]["missing_fields"]["_value"].sort()
-            print("Warning -- '%s' is missing the following:" % self.full_path())
-            for i in range(len(missing_fields)):
-                print("\t" + missing_fields[i])
-        # report errors for missing mandatory data
-        if len(err_str) > 0:
-            print("TimeSeries creation error (name=%s; path=%s)" % (self.name, self.full_path()))
-            if len(err_str) == 1:
-                print("Missing mandatory field:")
-            else:
-                print("Missing mandatory field(s):")
-            for i in range(len(err_str)):
-                print("\t" + err_str[i])
-            sys.exit(1)
-        # TODO check _linkto
-
-        # make sure dataset or group doesn't already exist w/ this name
-        if self.full_path() in self.nwb.file_pointer:
-            self.fatal_error("HDF5 element %s already exists"%self.full_path())
-        # write content to file
-        grp = self.nwb.file_pointer.create_group(self.full_path())
-        self.nwb.write_datasets(grp, "", spec)
-
-        # allow freeing of memory
-        self.spec = None
-        # set done flag
-        self.finalized = True
+#    @deprecated
+#    def set_path(self, path):
+#        """ Sets the path for where the *TimeSeries* is created. This
+#            is only necessary for *TimeSeries* that were not created
+#            indicating the common storage location (ie, acquisition,
+#            stimulus, template) or that were not added to a module
+#        """
+#        if self.finalized:
+#            self.fatal_error("Added value after finalization")
+#        if path.endswith('/'):
+#            self.path = path
+#        else:
+#            self.path = path + "/"
+#        full_path = self.path + self.name
+#        if full_path in self.nwb.file_pointer:
+#            self.fatal_error("group '%s' already exists" % full_path)
+#
+#    @deprecated
+#    def full_path(self):
+#        """ Returns the HDF5 path to this *TimeSeries*
+#
+#            Arguments: 
+#                *none*
+#
+#            Returns:
+#                *nothing*
+#        """
+#        return self.path + self.name
+#
+#    @deprecated
+#    def finalize(self):
+#        """ Finish the *TimSeries* and write pending changes to disk
+#
+#            Arguments:
+#                *none*
+#
+#            Returns:
+#                *nothing*
+#        """
+#        if self.finalized:
+#            return
+#        # verify path is in acceptable location
+#        # AJTRITT: BEGIN validation
+#        valid_loc = False   # ever the pessimest
+#        template = False    
+#        if self.path.startswith("/acquisition/timeseries"):
+#            valid_loc = True
+#        elif self.path.startswith("acquisition/timeseries"):
+#            valid_loc = True
+#        elif self.path.startswith("/stimulus/templates"):
+#            valid_loc = True
+#            template = True
+#        elif self.path.startswith("stimulus/templates"):
+#            valid_loc = True
+#            template = True
+#        elif self.path.startswith("/stimulus/presentation"):
+#            valid_loc = True
+#        elif self.path.startswith("stimulus/presentation"):
+#            valid_loc = True
+#        elif self.path.startswith("/analysis"):
+#            valid_loc = True
+#        elif self.path.startswith("analysis"):
+#            valid_loc = True
+#        elif self.path.startswith("/processing") and len(self.path) > len("/processing/"):
+#            valid_loc = True
+#        elif self.path.startswith("processing") and len(self.path) > len("processing/"):
+#            valid_loc = True
+#        if not valid_loc:
+#            print("Timeseries '%s' is not stored in a correct location" % self.name)
+#            print("Specified path is '%s'" % self.path)
+#            print("Possible solutions:")
+#            print("1) when creating the time series, specify modality='acquisition', ")
+#            print("   'stimulus' or 'template' and the path will be correctly")
+#            print("   assigned")
+#            print("2) add the time series to a processing module")
+#            print("3) manually assign a valid directory in '/acquisition/',")
+#            print("   'stimulus', 'processing' or 'analysis' folders")
+#            self.nwb.fatal_error("Invalid TimeSeries path")
+#        # AJTRITT: END validation
+#
+#        valid_loc = False   # ever the pessimest
+#        from . import nwb as nwblib
+#        nwblib.register_finalization(self.path + self.name, self.serial_num)
+#        # tell kernel about link so table of all links can be added to
+#        #   file at end
+#        if self.data_tgt_path_soft is not None:
+#            self.nwb.record_timeseries_data_soft_link(self.full_path(), self.data_tgt_path_soft)
+#        if self.data_tgt_path is not None:
+#            self.nwb.record_timeseries_data_link(self.full_path(), self.data_tgt_path)
+#        if self.time_tgt_path is not None:
+#            self.nwb.record_timeseries_time_link(self.full_path(), self.time_tgt_path)
+#        # verify all mandatory fields are present
+#        # AJTRITT: BEGIN validation
+#        spec = self.spec
+#        if template:    # VALIDATOR
+#            spec["timestamps"]["_include"] = "optional"
+#            spec["starting_time"]["_include"] = "optional"
+#        # num_samples can sometimes be calculated automatically. do so
+#        #   here if that's possible
+#        if "_value" not in spec["num_samples"]:
+#            if "_value" in spec["timestamps"]:
+#                # make tmp short name to avoid passing 80-col limit in editor
+#                tdat = spec["timestamps"] 
+#                spec["num_samples"]["_value"] = len(tdat["_value"])
+#        # document missing standard fields
+#        err_str = []
+#        missing_fields = []
+#        for k in list(spec.keys()):
+#            if k.startswith('_'):   # check for leading underscore
+#                continue    # control field -- ignore
+#            if "_value" in spec[k]:
+#                continue    # field exists
+#            if spec[k]["_include"] == "required":
+#                # value is missing -- see if alternate or link exists
+#                if "_value_softlink" in spec[k]:
+#                    continue
+#                if "_value_hardlink" in spec[k]:
+#                    continue
+#                # valid alternative fields include links -- check possibilities
+#                if "_alternative" in spec[k]:
+#                    if "_value" in spec[spec[k]["_alternative"]]:
+#                        continue    # alternative field exists
+#                    if "_value_hardlink" in spec[spec[k]["_alternative"]]:
+#                        continue    # alternative field exists
+#                    if "_value_softlink" in spec[spec[k]["_alternative"]]:
+#                        continue    # alternative field exists
+#                miss_str = "Missing field '%s'" % k
+#                if "_alternative" in spec[k]:
+#                    miss_str += " (or '%s')" % spec[k]["_alternative"]
+#                err_str.append(str(miss_str))
+#            # make a record of missing required fields
+#            if spec[k]["_include"] == "standard":
+#                if "_value" not in spec["_attributes"]["missing_fields"]:
+#                    spec["_attributes"]["missing_fields"]["_value"] = []
+#                spec["_attributes"]["missing_fields"]["_value"].append(str(k))
+#                missing_fields.append(str(k))
+#        # add spec's _description to 'help' field
+#        # use asserts -- there's a problem w/ the spec if these don't exist
+#        assert "help" in spec["_attributes"]
+#        assert "_description" in spec
+#        spec["_attributes"]["help"]["_value"] = spec["_description"]
+#        # make sure that mandatory attributes are present
+#        lspec = []
+#        lspec.append(spec)
+#        if "_value" in spec["data"]:
+#            lspec.append(spec["data"])
+#        if "_value" in spec["timestamps"]:
+#            lspec.append(spec["timestamps"])
+#        if "_value" in spec["starting_time"]:
+#            lspec.append(spec["starting_time"])
+#        for i in range(len(lspec)):
+#            for k in lspec[i]["_attributes"]:
+#                if lspec[i]["_attributes"][k]["_include"] == "required":
+#                    if "_value" not in lspec[i]["_attributes"][k]:
+#                        err_str.append("Missing attribute: " + k)
+#        # AJTRITT: END validation
+#
+#        # report missing standard data
+#        if len(missing_fields) > 0:
+#            missing_fields.sort()
+#            spec["_attributes"]["missing_fields"]["_value"].sort()
+#            print("Warning -- '%s' is missing the following:" % self.full_path())
+#            for i in range(len(missing_fields)):
+#                print("\t" + missing_fields[i])
+#        # report errors for missing mandatory data
+#        if len(err_str) > 0:
+#            print("TimeSeries creation error (name=%s; path=%s)" % (self.name, self.full_path()))
+#            if len(err_str) == 1:
+#                print("Missing mandatory field:")
+#            else:
+#                print("Missing mandatory field(s):")
+#            for i in range(len(err_str)):
+#                print("\t" + err_str[i])
+#            sys.exit(1)
+#        # TODO check _linkto
+#
+#        # make sure dataset or group doesn't already exist w/ this name
+#        if self.full_path() in self.nwb.file_pointer:
+#            self.fatal_error("HDF5 element %s already exists"%self.full_path())
+#        # write content to file
+#        grp = self.nwb.file_pointer.create_group(self.full_path())
+#        self.nwb.write_datasets(grp, "", spec)
+#
+#        # allow freeing of memory
+#        self.spec = None
+#        # set done flag
+#        self.finalized = True
 
 
 
@@ -745,6 +745,9 @@ class ElectricalSeries(TimeSeries):
         """
         super().set_data(data, "volt", conversion=conversion, resolution=resolution)
 
+    def set_electrode(self, idx, name, **metadata):
+        pass
+
     def set_electrodes(self, electrodes):
         """ Specify the electrodes that this corresponds to in the electrode
             map.
@@ -753,6 +756,12 @@ class ElectricalSeries(TimeSeries):
                 *names* (int array) The electrode indices
         """
         self.electrodes = electrodes
+
+
+class ElectrodeGroup(Container):
+    """Container type for  <electrode_group_X>
+    """
+    pass
     
 
 class SpikeEventSeries(ElectricalSeries):
