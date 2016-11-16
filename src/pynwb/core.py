7b6ccca8938b520a6940f94be92e58e5ed00e934
Andrew Tritt <ajtritt@lbl.gov>
2016-11-15 21:27:16 -0800

    moved ExtenderMeta, Container, and docval decorator to core.py

--- src/pynwb/core.py
+++ src/pynwb/core.py
@@ -1,144 +1,188 @@
-'''This submodule will contain classes and objects for validating
-the NWB format schema.
-'''
+import itertools as _itertools
+import copy as _copy
+import abc 
 
-from .h5tools import h5_scalar, sync_files
-from .nwbtools import find_timeseries 
+def __type_okay(value, argtype):
+    return True
 
-# A dictionary for storing the format schema
-# We will initialize this in __init__
-__schema__ = {}
-    
-#class NeurodataFile(ImmutableGroup, h5py.File):
-class NeurodataFile(h5py.File):
-    """This class serves as a wrapper for an h5py.File object
-    that enforces NWB specifications at the top-level. For, example
-    this is where we enforce the existence of the group "general"
-    """
-    
-    def __init__(self, path, mode='r', master=None):
-        """Open a new NWBFile for reading
-
-        :param path: The path to the NWB file to open
-        :type path: str.
-        :param mode: The mode to open the file in
-        :type path: str.
-        :param master: The master file to create a subfile to. Use this for 
-                       'appending' to existing files. This is useful for 
-                       adding analysis results to raw data files
-        :type master: NeurodataFile.
-
-
-        :raises: NWBParseError
-        """
-
-        super(filename, mode)
-        if master:
-            master.add_data_file(self)
-        # TODO: This is just a placeholder to convey what we want to do here
-        # When validation code is established, the actual function name and use
-        # needs to be added
-        if not _schema.validate_nwb(self):
-            raise NWBSyntaxError()
-        self._create_date = [datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S") for date_str in self['file_create_date']]
-        self._start_time = datetime.strptime(self['session_start_time'], "%Y-%m-%dT%H:%M:%S")
-        self._timeseries = dict()
-        
-
-    def add_data_file(self, child):
-        """Connect child to self via appropriate links. 
-        Use this to add a new file to a collection of NWB files 
-        """
-        sync_files(self, child)
-        
-
-
-    def add_timeseries(self, timeseries):
-        pass
-
-
-    def create_raw_timeseries(*args, **kwargs):
-        """
-            timeseries_type, comments, attributes
-        """
-        ts_type = args[0]
-        name = args[1]
-        data = args[2]
-        comments = args[3]
-        validator = getattr(nwbtools.validators, ts_type)
-        object_specs = list()
-        attributes = dict()
-        # do some stuff to build the dict needed for writing with h5tools.write_group
-        group = self.create_group(name)
-        missing_specs = validator.validate(object_specs, attributes)
-        if not len(missing_specs):
-            h5tools.write_group(group, object_specs, attributes)
-
-    def create_analysis_result(*args, **kwargs):
-        pass
-        
-    @property
-    @h5_scalar
-    def nwb_version(self):
-        """The NWB version for this file
-        """
-        return self['nwb_version']
-
-    @property
-    def modification_dates(self):
-        """The modification dates of this file. The 0-element will be the file creation date.
-        """
-        return self._create_date
-
-    @property
-    @h5_scalar
-    def description(self):
-        """A description of the experiment and data in this file
-        """
-        return self['session_description']
-
-    @property
-    @h5_scalar
-    def identifier(self):
-        """The identifier for this NWB file
-        """
-        return self['identifier']
-
-    @property
-    def start_time(self):
-        """A description of the experiment and data in this file
-        """
-        return self._start_time
-    
-    def fetch_timeseries(self, *args, **kwargs):
-        '''Return the TimeSeries objects corresponding to the given name
-        
-        :param name: the name of the 
-        
-        '''
-        if name in self._timeseries:
-            if len(self._timeseries[name]):
-            return self._timeseries[name]
+def __parse_args(validator, args, kwargs, enforce_type=True):
+    ret = dict()
+    errors = list()
+    argi = 0
+    for arg in validator:
+        argname = arg['name']
+        # check if this is a positional argument or not
+        if 'default' in arg:
+            # this is a key-value arg
+            if argname in kwargs:
+                ret[argname] = kwargs[argname]
+            elif len(args) > argi:
+                ret[argname] = args[argi]
+                argi += 1
+            else:
+                ret[argname] = arg['default']
         else:
-            result = self.visit
-            
-            #
-            return None
+            # this is a positional arg
+            ret[argname] = args[argi]
+            argi += 1
+        if not __type_okay(ret[argname], arg['type']):
+            errors.append(argname)
     
-    def close(self):
-        '''Validate that this HDF5 file is a valid NWB file, and then close'''
-        super().close()
+    return {'args': ret, 'errors': errors}
 
-class TimeSeries(ImmutableGroup):
+def __sort_args(validator):
+    pos = list()
+    kw = list()
+    for arg in validator:
+        if "default" in arg:
+            kw.append(arg)
+        else:
+            pos.append(arg)
+    return list(_itertools.chain(pos,kw))
 
-    def __init__(self, ):
-        pass
+docval_attr_name = 'docval'
+
+def docval(*validator, **options):
+    enforce_type = options.pop('enforce_type', False)
+    validator = __sort_args(validator)
+    def dec(func):
+        _docval = _copy.copy(options)
+        _docval['args'] = validator
+        def func_call(*args, **kwargs):
+            self = args[0]
+            parsed = __parse_args(validator, args[1:], kwargs, enforce_type=enforce_type)
+            parse_err = parsed.get('errors')
+            if parse_err:
+                # TODO: handle parse errors
+                pass
+            return func(self, **parsed['args'])
+        setattr(func_call, docval_attr_name, _docval)
+        return func_call
+    return dec
+
+def getargs(*args, **kwargs):
+    for arg in args:
+        yield kwargs[arg]
+
+def docstring(func):
+    def __sphinx_args(validator):
+        tmp = copy.copy(validator)
+        if isinstance(tmp['type'], type):
+            tmp['type'] = tmp['type'].__name__
+        tmpl = (":param {name}: {doc}\n"
+                ":type  {name}: {type}")
+    return tmpl.format(**validator)
+    
+    if hasattr(func, 'validator'):
+        validator = func.validator
+    else:
+        return None
+    pos = list()
+    kw = list()
+    sig =  ".. py:function:: %s(%s)\n\n" % (func.__name__, ", ".join(x['name'] for x in validator)) 
+    sig += "%s\n\n" % func.__doc__
+    sig += "\n".join(map(__sphinx_args, func.docval['args']))
+
+class Container(object):
+    
+    @docval({'name': 'parent', 'type': 'Container', 'doc': 'the parent Container for this Container', 'default': None},
+            {'name': 'container_source', 'type': object, 'doc': 'the source of this Container e.g. file name', 'default': None},
+            enforce_type=False)
+    def __init__(self, **kwargs):
+        parent, container_source = getargs('parent', 'container_source', **kwargs)
+        self.__fields = list()
+        self.__subcontainers = list()
+        self.__parent = None
+        if parent:
+            self.parent = parent
+        self.__container_source = container_source
+
+    @property
+    def container_source(self):
+        return self.__container_source
     
     @property
-    @h5_scalar
-    def comments(self):
-        return self['comments']
+    def fields(self):
+        return self.__fields
     
+    @property
+    def subcontainers(self):
+        return self.__subcontainers
+
+    @property
+    def parent(self):
+    #def get_parent(self):
+        return self.__parent
     
-for prop in ('comments', 'description', 'help', 'source'):
-    setattr(TimeSeries, prop, property(h5_scalar(lambda self: self[prop])))
+    @parent.setter
+    def parent(self, parent_container):
+        if self.__parent:
+            self.__parent.__subcontainers.remove(self)
+        self.__parent = parent_container
+        parent_container.__subcontainers.append(self)
+
+    
+    #@ExtenderMeta.pre_init
+    #def print_bases(cls):
+    #    print("%s %s" % (cls.__name__, cls.__base__))
+    #    base_cls = cls.__base__
+    #    for name, func in filter(lambda tup: hasattr(tup[1], docval_attr_name), cls.__dict__.items()):
+    #        docval = getattr(func, docval_attr_name)
+    #        if hasattr(base_cls, name):
+    #            print("base class has %s" % (name))
+    #            base_func = getattr(base_cls, name)
+    #            if hasattr(base_func, docval_attr_name):
+    #                print("%s has docval in base class" % (name))
+    #                
+    #                base_docval = getattr(base_func, docval_attr_name)
+    #                vdict = _collections.OrderedDict()
+    #                for arg in base_docval['args']:
+    #                    vdict[arg['name']] = arg
+    #                for arg in docval['args']:
+    #                    vdict[arg['name']] = arg
+    #                docval['args'] = list(vdict.values())
+    #
+    #        def func_call(*args, **kwargs):
+    #            self = args[0]
+    #            parsed = parse_args(docval['args'], args[1:], kwargs, enforce_type=docval.get('enforce_type', False))
+    #            parse_err = parsed.get('errors')
+    #            if parse_err:
+    #                # TODO: handle parse errors
+    #                pass
+    #            return func(self, **parsed['args'])
+    #        print("%s %s" % (name, docval))
+    #        setattr(cls, name, func_call)
+
+
+class ExtenderMeta(abc.ABCMeta):
+    """A metaclass that will extend the base class initialization
+       routine by executing additional functions defined in 
+       classes that use this metaclass
+        
+       In general, you should not use this class, unless your name is
+       Andrew James Tritt.
+    """
+
+    #__preinit = '__preinit'
+    #@classmethod
+    #def pre_init(cls, func):
+    #    setattr(func, cls.__preinit, True)
+    #    return classmethod(func)
+
+    __postinit = '__postinit'
+    @classmethod
+    def post_init(cls, func):
+        setattr(func, cls.__postinit, True)
+        return classmethod(func)
+
+    def __init__(cls, name, bases, classdict):
+        #it = (getattr(cls, n) for n in dir(cls))
+        #it = (a for a in it if hasattr(a, cls.__preinit))
+        #for func in it:
+        #    func()
+        super(ExtenderMeta, cls).__init__(name, bases, classdict)
+        it = (getattr(cls, n) for n in dir(cls))
+        it = (a for a in it if hasattr(a, cls.__postinit))
+        for func in it:
+            func()
+
