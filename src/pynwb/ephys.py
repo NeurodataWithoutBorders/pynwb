70b94049c7626e85c41f695814f44a20e2c5e6ad
Andrew Tritt <ajtritt@lbl.gov>
2016-11-15 23:12:45 -0800

    updated ephys.ElectrodeGroup to use helper functions and added unit tests

--- src/pynwb/ephys.py
+++ src/pynwb/ephys.py
@@ -1,41 +1,30 @@
-from .container import Container
+from .core import Container, properties, docval, getargs
 
+
+
+
+__std_fields = ('name',
+                'description',
+                'device',
+                'location',
+                'physical_location',
+                'impedance')
+@properties(*__std_fields)
 class ElectrodeGroup(Container):
-    def __init__(self, name, coord, desc, dev, loc, parent=None):
-        super().__init__(parent)
-        self._coord = coord
-        self._description = description
-        self._device = device
-        self._location = location
 
-    @property
-    def description(self):
-        return self._description
-
-    @description.setter
-    def set_description(self, description):
-        self._description = description
-
-    @property
-    def device(self):
-        return self._device
-
-    @device.setter
-    def set_device(self, device):
-        self._device = device
-
-    @property
-    def location(self):
-        return self._location
-
-    @location.setter
-    def set_location(self, location):
-        self._location = location
-
-    @property
-    def physical_location(self):
-        return self._coord
-
-    def set_physical_location(self, x, y, z):
-        self._coord = (x,y,z)
-
+    @docval(*Container.__init__.docval['args'],
+            {'name': 'name', 'type': (str, int), 'doc': 'the name of this electrode'},
+            {'name': 'coord', 'type': tuple, 'doc': 'the x,y,z coordinates of this electrode'},
+            {'name': 'desc', 'type': str, 'doc': 'a description for this electrode'},
+            {'name': 'dev', 'type': str, 'doc': 'the device this electrode was recorded from on'},
+            {'name': 'loc', 'type': str, 'doc': 'a description of the location of this electrode'},
+            {'name': 'imp', 'type': float, 'doc': 'the impedance of this electrode', 'default': -1})
+    def __init__(self, **kwargs):
+        name, coord, desc, dev, loc, imp, parent = getargs("name", "coord", "desc", "dev", "loc", "imp", "parent", **kwargs)
+        super(ElectrodeGroup, self).__init__(parent=parent)
+        self._name = name
+        self._physical_location = coord
+        self._description = desc
+        self._device = dev
+        self._location = loc
+        self._impedance = imp
---
+++ tests/test_ephys.py
@@ -0,0 +1,38 @@
+import unittest
+from context import pynwb
+
+from pynwb.ephys import *
+
+class TestElectrodeGroupConstructor(unittest.TestCase):
+
+    def setUp(self):
+        #print(ElectrodeGroup.__init__.docval)
+        self.name = 'my_group'
+        self.coord = (1.1, 2.2, 3.3)
+        self.desc = "an example electrode group"
+        self.dev = "my favorite device"
+        self.loc = "my favorote brain location"
+        self.eg = ElectrodeGroup(self.name, 
+                                 self.coord,
+                                 self.desc,
+                                 self.dev,
+                                 self.loc)
+
+
+    def test_impedance(self):
+        self.assertEqual(self.eg.impedance, -1)
+        
+    def test_name(self):
+        self.assertEqual(self.eg.name, self.name)
+
+    def test_physical_location(self):
+        self.assertTupleEqual(self.eg.physical_location, self.coord)
+
+    def test_description(self):
+        self.assertEqual(self.eg.description, self.desc)
+
+    def test_device(self):
+        self.assertEqual(self.eg.device, self.dev)
+
+    def test_location(self):
+        self.assertEqual(self.eg.location, self.loc)
