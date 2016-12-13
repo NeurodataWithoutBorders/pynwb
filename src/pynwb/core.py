648c5cab9ac729e239e7778a5d31bb9a26af9f2f
Andrew Tritt <ajtritt@lbl.gov>
2016-12-12 19:56:29 -0800

    moved Container class own module

--- src/pynwb/core.py
+++ src/pynwb/core.py
@@ -2,9 +2,9 @@ import itertools as _itertools
 import copy as _copy
 import abc 
 
-def __type_okay(value, argtype):
+def __type_okay(value, argtype, allow_none=False):
     if value is None:
-        return False
+        return allow_none
     if isinstance(argtype, str):
         return argtype in [cls.__name__ for cls in value.__class__.__mro__]
     elif isinstance(argtype, type):
@@ -41,6 +41,7 @@ def __parse_args(validator, args, kwargs, enforce_type=True):
             skip_enforce_type = False
             if argname in kwargs:
                 ret[argname] = kwargs[argname]
+                #skip_enforce_type = ret[argname] is None and arg['default'] is None
             elif len(args) > argsi:
                 ret[argname] = args[argsi]
                 argsi += 1
@@ -48,10 +49,10 @@ def __parse_args(validator, args, kwargs, enforce_type=True):
                 ret[argname] = arg['default']
                 # if default is None, skip the argument check
                 # Note: this line effectively only allows NoneType for defaults
-                skip_enforce_type = arg['default'] is None
+                #skip_enforce_type = arg['default'] is None
             if not skip_enforce_type and enforce_type:
                 argval = ret[argname]
-                if not __type_okay(argval, arg['type']):
+                if not __type_okay(argval, arg['type'], arg['default'] is None):
                     fmt_val = (argname, type(argval).__name__, __format_type(arg['type']))
                     errors.append("incorrect type for '%s' (got '%s', expected '%s')" % fmt_val)
         # this is a positional arg
@@ -104,7 +105,10 @@ def getargs(*args, **kwargs):
     for arg in args:
         yield kwargs[arg]
 
-def docstring(func):
+def huread_doc(func):
+    pass
+
+def sphinxdoc(func):
     def __sphinx_args(validator):
         tmp = copy.copy(validator)
         if isinstance(tmp['type'], type):
@@ -142,43 +146,6 @@ def properties(*props):
             classdict[arg] = property(getter)
         return type(cls.__name__, cls.__bases__, classdict)
     return outer
-
-class Container(object):
-    
-    @docval({'name': 'parent', 'type': 'Container', 'doc': 'the parent Container for this Container', 'default': None},
-            {'name': 'container_source', 'type': object, 'doc': 'the source of this Container e.g. file name', 'default': None})
-    def __init__(self, **kwargs):
-        parent, container_source = getargs('parent', 'container_source', **kwargs)
-        self.__fields = list()
-        self.__subcontainers = list()
-        self.__parent = None
-        if parent:
-            self.parent = parent
-        self.__container_source = container_source
-
-    @property
-    def container_source(self):
-        return self.__container_source
-    
-    @property
-    def fields(self):
-        return self.__fields
-    
-    @property
-    def subcontainers(self):
-        return self.__subcontainers
-
-    @property
-    def parent(self):
-    #def get_parent(self):
-        return self.__parent
-    
-    @parent.setter
-    def parent(self, parent_container):
-        if self.__parent:
-            self.__parent.__subcontainers.remove(self)
-        self.__parent = parent_container
-        parent_container.__subcontainers.append(self)
 
     
     #@ExtenderMeta.pre_init
