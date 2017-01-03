0778c9dd868910e1033488857e5e74a8978f7fd1
Andrew Tritt <ajtritt@lbl.gov>
2017-01-03 15:33:17 -0800

    added docval to SpatialSeries constructor

--- src/pynwb/ui/timeseries.py
+++ src/pynwb/ui/timeseries.py
@@ -453,14 +453,30 @@ class SpatialSeries(TimeSeries):
     
     _help = "Stores points in space over time. The data[] array structure is [num samples][num spatial dimensions]"
 
-    def __init__(self, name, reference_frame=None):
+    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
+            {'name': 'reference_frame', 'type': str, 'doc': 'description defining what the zero-position is'},
+            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
+                                                   'contained here. It can also be the name of a device, for stimulus or '
+                                                   'acquisition data')},
+            {'name': 'data', 'type': (list, np.ndarray), 'doc': 'The data this TimeSeries dataset stores. Can also store binary data e.g. image frames', 'default': None},
+            {'name': 'conversion', 'type': float, 'doc': 'Scalar to multiply each element by to conver to volts', 'default': _default_conversion},
+            {'name': 'resolution', 'type': float, 'doc': 'The smallest meaningful difference (in specified unit) between values in data', 'default': _default_resolution},
+            {'name': 'timestamps', 'type': (list, np.ndarray), 'doc': 'Timestamps for samples stored in data', 'default': None},
+            {'name': 'starting_time', 'type': float, 'doc': 'The timestamp of the first sample', 'default': None},
+            {'name': 'rate', 'type': float, 'doc': 'Sampling rate in Hz', 'default': None},
+            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
+            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
+            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
+            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
+            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
+            )
+    def __init__(self, **kwargs):
         """Create a SpatialSeries TimeSeries dataset
-            Arguments:
-                *reference_frame* (string) Description defining what exactly 'straight-ahead' means.
         """
+        name, reference_frame = getargs('name', 'reference_frame', **kwargs)
         super().__init__(name)
         self.reference_frame = reference_frame
 
-    def set_data(self, data, conversion=None, resolution=None):
+    def set_data(self, data, conversion=_default_conversion, resolution=_default_resolution):
         super().set_data(data, "meter", conversion=conversion, resolution=resolution)
 
