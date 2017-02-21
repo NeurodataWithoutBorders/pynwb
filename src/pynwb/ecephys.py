99bdd9bd3764b0d2a6ba08bbc344211f60801f3e
Andrew Tritt <ajtritt@lbl.gov>
2017-02-21 10:44:33 -0800

    cleaned up some Module classes in ecephys module

--- src/pynwb/ecephys.py
+++ src/pynwb/ecephys.py
@@ -1,5 +1,5 @@
 from .base import TimeSeries, Interface, _default_resolution, _default_conversion
-from .core import docval, getargs, NWBContainer
+from .core import docval, getargs, popargs, NWBContainer
 
 import numpy as np
 from collections import Iterable
@@ -9,7 +9,7 @@ class ElectricalSeries(TimeSeries):
     __nwbfields__ = ('electrodes',)
 
     _ancestry = "TimeSeries,ElectricalSeries"
-    _help = "Stores acquired voltage data from extracellular recordings"
+    __help = "Stores acquired voltage data from extracellular recordings"
 
     @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
             {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
@@ -45,7 +45,7 @@ class SpikeEventSeries(ElectricalSeries):
 
     _ancestry = "TimeSeries,ElectricalSeries,SpikeSeries"
 
-    _help = "Snapshots of spike events from data."
+    __help = "Snapshots of spike events from data."
 
     @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
             {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
@@ -81,7 +81,7 @@ class ElectrodeGroup(NWBContainer):
                      'impedance')
 
     @docval({'name': 'name', 'type': (str, int), 'doc': 'the name of this electrode'},
-            {'name': 'coord', 'type': tuple, 'doc': 'the x,y,z coordinates of this electrode'},
+            {'name': 'coord', 'type': (tuple, list, np.ndarray), 'doc': 'the x,y,z coordinates of this electrode'},
             {'name': 'desc', 'type': str, 'doc': 'a description for this electrode'},
             {'name': 'dev', 'type': str, 'doc': 'the device this electrode was recorded from on'},
             {'name': 'loc', 'type': str, 'doc': 'a description of the location of this electrode'},
@@ -100,39 +100,44 @@ class EventDetection(Interface):
     __nwbfields__ =  ('detection_method',
                       'source_indices',
                       'event_times',
-                      'voltage_data')
+                      'data')
 
     _help_statement = ("Description of how events were detected, such as voltage "
                        "threshold, or dV/dT threshold, as well as relevant values.")
 
-    _interface = "EventDetection"
-
-    def __init___(self, voltage_data_ts, detection_method,
-                  source_indices=list(), event_times=list()):
-        super(EventDetection, self).__init__()
-        self._detection_method = detection_method
-        self._source_indices = source_indices
-        self._event_times = event_times
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'data', 'type': ElectricalSeries, 'doc': 'the source electrophysiology data'},
+            {'name': 'detection_method', 'type': str, 'doc': 'a description of how events were detected'},
+            {'name': 'event_times', 'type': str, 'doc': 'timestamps of events'})
+    def __init___(self, **kwargs):
+                  #source_indices=list()):
+        source, data, detection_method, event_times = getargs('source', 'voltage_data_ts', 'detection_method', 'event_times', kwargs)
+        super(EventDetection, self).__init__(source)
+        self.detection_method = detection_method
+        self.event_times = event_times
+        self.source_indices = list() # I think we should calculate this under the hood
         # do not set parent, since this is a link
-        self._voltage_data = voltage_data_ts
+        self.data = data
 
-    def add_event(self, idx, time):
-        self._source_indices.append(idx)
-        self._event_time.append(time)
+    #def add_event(self, idx, time):
+    #    self.source_indices.append(idx)
+    #    self.event_time.append(time)
 
 class EventWaveform(Interface):
-    """Spike data for spike events
+    """Spike data for spike events detected in raw data
+    stored in this NWBFile, or events detect at acquisition
     """
-    __nwbfields__ = ('spike_data',)
+    __nwbfields__ = ('data',)
 
-    _help = "Waveform of detected extracellularly recorded spike events"
+    __help = "Waveform of detected extracellularly recorded spike events"
 
-    _interface = "EventWaveform"
-
-    def __init__(self, spike_data_ts=None):
-        super(EventWaveform, self).__init__()
-        self._spike_data = spike_data_ts
-        self._spike_data.parent = self
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'data', 'type': SpikeEventSeries, 'doc': 'spiking event data'})
+    def __init__(self, **kwargs):
+        source, data = getargs('source', 'spike_data_ts', kwargs)
+        super(EventWaveform, self).__init__(source)
+        spike_data_ts.parent = self
+        self.data = data
         
 
 class Clustering(Interface):
@@ -141,117 +146,93 @@ class Clustering(Interface):
        RMS on any channel in cluster.
     """
     __nwbfields__  = ('cluster_times',
-                      'cluster_ids')
+                      'cluster_ids',
+                      'peak_over_rms')
     
 
-    _help = ("Clustered spike data, whether from automatic clustering " 
+    __help = ("Clustered spike data, whether from automatic clustering " 
              "tools (eg, klustakwik) or as a result of manual sorting")
 
-    _interface = "Clustering"
 
-
-    def __init__(self, cluster_times=list(), 
-                 cluster_ids=list(), peak_over_rms=list()):
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'cluster_times', 'type': (tuple, list, np.ndarray), 'doc': 'times of clustered events'},
+            {'name': 'cluster_ids', 'type': (tuple, list, np.ndarray), 'doc': 'description of clusters and/or clustering method'},
+            {'name': 'peak_over_rms', 'type': (tuple, list, np.ndarray), 'doc': 'maximum ratio of waveform peak to RMS on any channel in the cluster'})
+    def __init__(self, **kwargs):
         """ Conveninece function to set interface values. Includes
             sanity checks for array lengths
-
-            Arguments:
-                *times* (double array) Times of clustered events, in
-                seconds. This may be a link to times field in associated
-                FeatureExtraction module. Array structure: [num events]
-
-                *num* (int array) Cluster number for each event Array 
-                structure: [num events]
-
-                *description* (text)  Description of clusters or 
-                clustering (e.g., cluster 0 is electrical noise, 
-                clusters curated using Klusters, etc)
-
-                *peak_over_rms* (float array) Maximum ratio of waveform 
-                peak to RMS on any channel in the cluster (provides a 
-                basic clustering metric).  Array structure: [num clusters]
-
-            Returns:
-                *nothing*
         """
-        super(Clustering, self).__init__()
-        self._cluster_times = cluster_times
-        self._cluster_ids = cluster_ids
-        self._peak_over_rms = dict(enumerate(peak_over_rms))
+        source, cluster_times, cluster_ids, peak_over_rms = getargs('source', 'cluster_times', 'cluster_ids', 'peak_over_rms', kwargs)
+        super(Clustering, self).__init__(source)
+        self.cluster_times = cluster_times
+        self.cluster_ids = cluster_ids
+        self.peak_over_rms = dict(enumerate(peak_over_rms))
 
-    def add_event(self, cluster_id, time):
-        self.nums.append(cluster_num)
-        self.times.append(time)
-
-    def add_cluster(self, cluster_num, peak_over_rms):
-        self.peak_over_rms[cluster_num] = peak_over_rms
+#    def add_event(self, cluster_id, time):
+#        self.nums.append(cluster_num)
+#        self.times.append(time)
+#
+#    def add_cluster(self, cluster_num, peak_over_rms):
+#        self.peak_over_rms[cluster_num] = peak_over_rms
 
 
 class ClusterWaveform(Interface):
     """Describe cluster waveforms by mean and standard deviation
        for at each sample.
     """
-    __nwbfields__ = ('filtering',
-                     'wf_mean',
-                     'wf_sd')
+    __nwbfields__ = ('clustering',
+                     'filtering',
+                     'means',
+                     'stdevs')
     
-    _help = ("Mean waveform shape of clusters. Waveforms should be "
+    __help = ("Mean waveform shape of clusters. Waveforms should be "
              "high-pass filtered (ie, not the same bandpass filter "
              "used waveform analysis and clustering)")
 
-    _interface = "ClusterWaveform"
-    
-    def __init__(self, clustering, filtering, wf_means=list(), wf_sds=list()):
-        super(ClusterWaveform, self).__init__()
-        self._clustering = clustering
-        self._filtering = filtering
-        self._wf_means = list()
-        self._wf_sds = list()
-
-    def add_waveform(self, sample_means, sample_sds):
-        self.wf_means.append(sample_means)
-        self.wf_sds.append(sample_sds)
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'clustering', 'type': Clustering, 'doc': 'the clustered spike data used as input for computing waveforms'},
+            {'name': 'filtering', 'type': str, 'doc': 'filter applied to data before calculating mean and standard deviation'},
+            {'name': 'means', 'type': Iterable, 'doc': 'the mean waveform for each cluster'},
+            {'name': 'stdevs', 'type': Iterable, 'doc': 'the standard deviations of waveforms for each cluster'})
+    def __init__(self, **kwargs):
+        source, clustering, filtering, means, stdevs = getargs('source', 'clustering', 'filtering', 'means', 'stdevs', kwargs)
+        super(ClusterWaveform, self).__init__(source)
+        self.clustering = clustering
+        self.filtering = filtering
+        self.means = means
+        self.sds = stdevs
 
 class LFP(Interface):
-    __nwbfields__ = ('lfp_data',)
 
-    _help = ("LFP data from one or more channels. Filter properties "
+    __nwbfields__ = ('data',)
+
+    __help = ("LFP data from one or more channels. Filter properties "
              "should be noted in the ElectricalSeries")
 
-    _interface = "LFP"
-
-    def __init__(self, lfp_ts=None):
-        super(LFP, self).__init__()
-        if lfp_ts:
-            self.add_lfp_data(lfp_ts)
-        else:
-            self._lfp_data = None
-
-    def add_lfp_data(self, lfp_ts):
-        self._lfp_data = lfp_ts
-        self._lfp_data.parent = self
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'data', 'type': ElectricalSeries, 'doc': 'LFP electrophysiology data'})
+    def __init__(self, **kwargs):
+        source, data = getargs('source', 'data', kwargs)
+        data.parent = self
+        super(LFP, self).__init__(source)
+        self.data = data
 
 class FilteredEphys(Interface):
 
-    __nwbfields__ = ('ephys_data',)
+    __nwbfields__ = ('data',)
 
-    _help = ("Ephys data from one or more channels that is subjected to filtering, such as "
+    __help = ("Ephys data from one or more channels that is subjected to filtering, such as "
              "for gamma or theta oscillations (LFP has its own interface). Filter properties should "
              "be noted in the ElectricalSeries")
 
-    _interface = "FilteredEphys"
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'data', 'type': ElectricalSeries, 'doc': 'filtered electrophysiology data'})
+    def __init__(self, **kwargs):
+        source, data = getargs('source', 'data', kwargs)
+        data.parent = self
+        super(FilteredEphys, self).__init__(source)
+        self.data = data
 
-    def __init__(self, ephys_ts=None):
-        super(FilteredEphys, self).__init__()
-        if ephys_ts:
-            self.add_ephys_data(ephys_ts)
-        else:
-            self._ephys_data = None
-
-    def add_lfp_data(self, ephys_ts):
-        self._ephys_data = ephys_ts
-        self._ephys_data.parent = self
-        
 
 class FeatureExtraction(Interface):
 
@@ -260,21 +241,25 @@ class FeatureExtraction(Interface):
                      'event_times',
                      'features')
 
-    _help = "Container for salient features of detected events"
+    __help = "Container for salient features of detected events"
 
-    _interface = "FeatureExtraction"
-
-    def __init__(self, electrodes, description, event_times=list(), features=list()):
-        super(FeatureExtraction, self).__init__()
-        self._electrodes = electrodes
-        self._description = description
-        self._event_times = event_times
-        self._features = features
-
-    def add_event_feature(self, time, features):
-        if len(features) != len(self._electrodes):
+    @docval({'name': 'source', 'type': str, 'doc': 'the source of the data represented in this Module Interface'},
+            {'name': 'electrodes', 'type': (list, tuple), 'doc': 'the electrode groups for each channel from which features were extracted'},
+            {'name': 'description', 'type': (list, tuple), 'doc': 'a description for each feature extracted'},
+            {'name': 'event_times', 'type': (list, tuple, np.ndarray), 'doc': 'the times of events that features correspond to'},
+            {'name': 'features', 'type': (list, tuple, np.ndarray), 'doc': 'features for each channel'})
+    def __init__(self, **kwargs):
+        source, electrodes, description, event_times, features = getargs('source', 'electrodes', 'description', 'event_times', 'features', kwargs)
+        if len(features) != len(electrodes):
             raise ValueError("incorrect dimensions: features -  must have one value per channel. Got %d, expected %d" % (len(features), len(self._electrodes)))
-        if len(features[0]) != len(self._description):
+        if len(features[0]) != len(description):
             raise ValueError("incorrect dimensions: features -  must have one value per feature. Got %d, expected %d" % (len(features[0]), len(self._description)))
-        self._features.append(features)
-        self._event_times.append(time)
+        super(FeatureExtraction, self).__init__(source)
+        self.fields['electrodes'] = electrodes
+        self.fields['description'] = description
+        self.fields['event_times'] = list(event_times)
+        self.fields['features'] = list(features)
+
+    #def add_event_feature(self, time, features):
+    #    self._features.append(features)
+    #    self._event_times.append(time)
