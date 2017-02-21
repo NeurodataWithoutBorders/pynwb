e6c2712bb1e9eea5f10217c9ad53a5cd259cded0
Andrew Tritt <ajtritt@lbl.gov>
2017-02-21 10:55:25 -0800

    added IntervalSeries class to misc module

--- src/pynwb/misc.py
+++ src/pynwb/misc.py
@@ -30,7 +30,7 @@ class AnnotationSeries(TimeSeries):
             {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
             {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
     )
-    def __init__(self, name, modality, spec, nwb):
+    def __init__(self, **kwargs):
         name, source, data, timestamps = getargs('name', 'source', 'data', 'timestamps', kwargs)
         super(AnnotationSeries, self).__init__(name, source, data, 'n/a', resolution=np.nan, conversion=np.nan, timestamps=timestamps)
 
@@ -96,7 +96,36 @@ class AbstractFeatureSeries(TimeSeries):
         self.data.append(features)
 
 class IntervalSeries(TimeSeries):
-    pass
+
+    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
+            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
+                                                   'contained here. It can also be the name of a device, for stimulus or '
+                                                   'acquisition data')},
+            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
+            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
+            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
+            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
+            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
+    )
+    def __init__(self, **kwargs):
+        name, source = getargs('name', 'source', kwargs)
+        self.__interval_data = list()
+        self.__interval_timestamps = list()
+        super(IntervalSeries, self).__init__(name, source, self.__interval_data, "n/a",
+                                             timestamps=self.__interval_timestamps,
+                                             conversion='nan',
+                                             resolution='nan',
+                                             **kwargs)
+
+    @docval({'name': 'start', 'type': float, 'doc': 'The name of this TimeSeries dataset'},
+            {'name': 'stop', 'type': float, 'doc': 'The name of this TimeSeries dataset'}
+    )
+    def add_interval(self, **kwargs):
+        start, stop = getargs('start', 'stop', kwargs)
+        self.__interval_timestamps.append(start)
+        self.__interval_timestamps.append(stop)
+        self.__interval_data.append(1)
+        self.__interval_data.append(-1)
 
 class UnitTimes(Interface):
 
