06702e0de7b5eddf4357690ec22c288d7a0684cf
Andrew Tritt <ajtritt@lbl.gov>
2017-03-29 20:19:32 -0700

    removed unnecessary whitespace, and old implementation of MotionCorrection

--- src/pynwb/image.py
+++ src/pynwb/image.py
@@ -62,7 +62,7 @@ class IndexSeries(TimeSeries):
     Stores indices to image frames stored in an ImageSeries. The purpose of the ImageIndexSeries is to allow
     a static image stack to be stored somewhere, and the images in the stack to be referenced out-of-order.
     This can be for the display of individual images, or of movie segments (as a movie is simply a series of
-    images). The data field stores the index of the frame in the referenced ImageSeries, and the timestamps 
+    images). The data field stores the index of the frame in the referenced ImageSeries, and the timestamps
     array indicates when that image was displayed.
     '''
 
@@ -105,7 +105,7 @@ class IndexSeries(TimeSeries):
 class ImageMaskSeries(ImageSeries):
     '''
     An alpha mask that is applied to a presented visual stimulus. The data[] array contains an array
-    of mask values that are applied to the displayed image. Mask values are stored as RGBA. Mask 
+    of mask values that are applied to the displayed image. Mask values are stored as RGBA. Mask
     can vary with time. The timestamps array indicates the starting time of a mask, and that mask
     pattern continues until it's explicitly changed.
     '''
@@ -154,9 +154,9 @@ class ImageMaskSeries(ImageSeries):
 
 class OpticalSeries(ImageSeries):
     '''
-    Image data that is presented or recorded. A stimulus template movie will be stored only as an 
+    Image data that is presented or recorded. A stimulus template movie will be stored only as an
     image. When the image is presented as stimulus, additional data is required, such as field of
-    view (eg, how much of the visual field the image covers, or how what is the area of the target 
+    view (eg, how much of the visual field the image covers, or how what is the area of the target
     being imaged). If the OpticalSeries represents acquired imaging data, orientation is also
     important.
     '''
@@ -235,80 +235,3 @@ class OpticalChannel(NWBContainer):
     # see /general/optophysiology/<imaging_plane_X>/<channel_X> spec
     pass
 
-class MotionCorrection(Interface):
-
-    iface_type = "MotionCorrection"
-
-    def add_corrected_image(self, name, orig, xy_translation, corrected):
-        """ Adds a motion-corrected image to the module, including
-            the original image stack, the x,y delta necessary to
-            shift the image frames for registration, and the corrected
-            image stack.
-            NOTE 1: All 3 timeseries use the same timestamps and so can share/
-            link timestamp arrays
-
-            NOTE 2: The timeseries passed in as 'xy_translation' and
-            'corrected' will be renamed to these names, if they are not
-            links to existing timeseries
-
-            NOTE 3: The timeseries arguments can be either TimeSeries
-            objects (new or old in case of latter 2 args) or strings.
-            If they are new TimeSeries objects, they will be stored
-            within the module. If they are existing objects, a link
-            to those objects will be created
-
-            Arguments:
-                *orig* (ImageSeries or text) ImageSeries object or
-                text path to original image time series
-
-                *xy_translation* TimeSeries storing displacements of
-                x and y direction in the data[] field
-
-                *corrected* Motion-corrected ImageSeries
-
-            Returns:
-                *nothing*
-        """
-        self.spec[name] = copy.deepcopy(self.spec["<>"])
-        # create link to original object
-        if isinstance(orig, _timeseries.TimeSeries):
-            if not orig.finalized:
-                self.nwb.fatal_error("Original timeseries must already be stored and finalized")
-            orig_path = orig.full_path()
-        else:
-            orig_path = orig
-        self.spec[name]["original"]["_value_hardlink"] = orig_path
-        self.spec[name]["original_path"]["_value"] = orig_path
-        links = "'original' is '%s'" % orig_path
-        # finalize other time series, or create link if a string was
-        #   provided
-        # XY translation
-        if isinstance(xy_translation, str):
-            self.spec[name]["xy_translation"]["_value_hardlink"] = xy_translation
-            links += "; 'xy_translation' is '%s'" % xy_translation
-        else:
-            if xy_translation.finalized:
-                # timeseries exists in file -- link to it
-                self.spec[name]["xy_translation"]["_value_hardlink"] = xy_translation.full_path()
-                links += "; 'corrected' is '%s'" % xy_translation.full_path()
-            else:
-                # new timeseries -- set it's path and finalize it
-                xy_translation.set_path("processing/" + self.module.name + "/MotionCorrection/" + name + "/")
-                xy_translation.reset_name("xy_translation")
-                xy_translation.finalize()
-        # corrected series
-        if isinstance(corrected, str):
-            self.spec[name]["corrected"]["_value_hardlink"] = corrected
-            links += "; 'corrected' is '%s'" % corrected
-        else:
-            if corrected.finalized:
-                # timeseries exists in file -- link to it
-                self.spec[name]["corrected"]["_value_hardlink"] = corrected.full_path()
-                links += "; 'corrected' is '%s'" % corrected.full_path()
-            else:
-                corrected.set_path("processing/" + self.module.name + "/MotionCorrection/" + name + "/")
-                corrected.reset_name("corrected")
-                corrected.finalize()
-        self.spec[name]["_attributes"]["links"]["_value"] = links
-
-########################################################################
