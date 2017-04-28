0c825a1144606733cd877ede5e48c122cd8286be
Andrew Tritt <ajtritt@lbl.gov>
2017-04-27 21:56:42 -0700

    committing current state of __init__.py

--- src/pynwb/spec/__init__.py
+++ src/pynwb/spec/__init__.py
@@ -14,52 +14,25 @@ def __get_resources():
     ret['data_dir_path'] = resource_filename(__name__, 'data')
     return ret
 
-def __build_namespaces(data_dir_path):
-    from glob import iglob
-    import ruamel.yaml as yaml
-    from itertools import chain
-    from os.path import join
-    from .catalog import SpecCatalog
-    spec_catalog = SpecCatalog()
-    exts = ['yaml', 'json']
-    glob_str = join(data_dir_path, "*.%s")
-    specs = dict()
-    namespaces = list()
-    # load all specs before resolving namespaces
-    for path in chain(*[iglob(glob_str % ext) for ext in exts]):
-        with open(path, 'r') as stream:
-            d  = yaml.safe_load(stream)
-            for k, v in d.items():
-                if k == 'specs':
-                    for spec_dict in v:
-                        spec_obj = GroupSpec.build_spec(spec_dict)
-                        if path not in specs:
-                            specs[path] = dict()
-                        specs[path][spec_obj.neurodata_type_def] = spec_obj
-
-                elif k == 'namespaces':
-                    namespaces.extend(v)
-    # build namespaces with SpecCatalogs
-    ret = dict()
-    for ns in namespaces:
-        catalog = SpecCatalog()
-        if 'schema' in ns:
-            for s in ns['schema']:
-                if 'source' not in s:
-                    continue
-                for spec in specs[s['source']]:
-                    ndts = spec.values()
-                    if 'neurodata_types' in s:
-                        ndts = filter(lambda x: x in set(s['neurodata_types']), ndts)
-                    for ndt_spec in ndts:
-                        catalog.auto_register(ndt_spec, s['source'])
-
-        ret[ns['name']] = SpecNamespace.build_namespace(**ns, catalog=catalog)
-
-
-    return ret
+def __get_namespace_catalog(data_dir_path, catalog):
+    namespaces = SpecNameSpace.build_namespace(data_dir_path)
+    for ns_name, ns in namespaces.items():
+        catalog.add_namespace(ns_name, ns)
 
 __resources = __get_resources()
 
-NAMESPACES = __build_namespaces(__resources['data_dir_path'])
 DEFAULT_NAMESPACE = 'core'
+NAMESPACES = __get_namespace_catalog(__resources['data_dir_path'], NamespaceCatalog())
+
+@docval({'name': 'name', 'type': str, 'doc': 'the name of this namespace'},
+        {'name': 'namespace', 'type': SpecNamespace, 'doc': 'the SpecNamespace object'},
+        is_method=False)
+def add_namespace(**kwargs):
+    name, namespace = getargs('name', 'namespace', kwargs)
+    NAMESPACES[name] = namespace
+
+@docval({'name': 'namespace_path', 'type': str, 'doc': 'the path to the YAML with the namespace definition'},
+        is_method=False)
+def load_namespace(**kwargs):
+    namespace_path = getargs('namespace_path', kwargs)
+    __get_namespace_catalog(namespace_path, NAMESPACES)
