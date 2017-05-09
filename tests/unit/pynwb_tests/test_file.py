a56f968865d8e69a2302fbe98ce83cb7f72c9b14
Andrew Tritt <ajtritt@lbl.gov>
2017-05-08 18:07:22 -0700

    added test for creating electrode group

--- tests/unit/pynwb_tests/test_file.py
+++ tests/unit/pynwb_tests/test_file.py
@@ -4,7 +4,7 @@ import unittest
 from datetime import datetime
 
 from pynwb import NWBFile
-from pynwb.ecephys import ElectrodeGroup, ElectricalSeries
+from pynwb.ecephys import Device
 
 class NWBFileTest(unittest.TestCase):
     def setUp(self):
@@ -29,6 +29,26 @@ class NWBFileTest(unittest.TestCase):
         self.assertEqual(self.nwbfile.experiment_description, 'a test experiment description')
         self.assertEqual(self.nwbfile.session_id, 'test1')
 
+
+    def test_create_electrode_group(self):
+        name = 'example_electrode_group'
+        ch_desc = [ 'the first channel', 'the second channel' ]
+        ch_loc = [ 'area 1', 'area2' ]
+        ch_filt = [ 'no filtering', 'no filtering' ]
+        ch_coord = [ [1.0, 1.0, 1.0], [2.0, 2.0, 2.0] ]
+        ch_imp = [-1, -1]
+        desc = 'An example electrode'
+        loc = 'an example location'
+        d = Device('a fake device')
+        elecgrp = self.nwbfile.create_electrode_group(name, ch_desc, ch_loc, ch_filt, ch_coord, ch_imp, desc, loc, d)
+        self.assertEqual(elecgrp.channel_description, ch_desc)
+        self.assertEqual(elecgrp.channel_coordinates, ch_coord)
+        self.assertEqual(elecgrp.channel_filtering, ch_filt)
+        self.assertEqual(elecgrp.channel_impedance, ch_imp)
+        self.assertEqual(elecgrp.channel_location, ch_loc)
+        self.assertEqual(elecgrp.description, desc)
+        self.assertEqual(elecgrp.location, loc)
+        self.assertIs(elecgrp.device, d)
 
 
 
