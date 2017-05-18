b9b19af4968cad11bec298d4d375f9514f0f2159
Andrew Tritt <ajtritt@lbl.gov>
2017-05-17 18:05:14 -0700

    moved base NWBContainer IO tester class into its own submodule

--- tests/unit/integration/ui_write/test_ui_write.py
+++ tests/unit/integration/ui_write/test_ui_write.py
@@ -8,7 +8,9 @@ from form.build import GroupBuilder, DatasetBuilder
 from form.backends.hdf5 import HDF5IO
 from pynwb import get_build_manager
 
-from pynwb import NWBContainer, NWBFile, TimeSeries
+from pynwb import NWBFile, TimeSeries
+
+from .base import TestNWBContainerIO
 
 CORE_NAMESPACE = 'core'
 
@@ -18,49 +20,6 @@ class SetEncoder(json.JSONEncoder):
             return list(o)
         else:
             return json.JSONEncoder.default(self, o)
-
-class TestNWBContainerIO(unittest.TestCase):
-
-    def setUp(self):
-        if type(self) == TestNWBContainerIO:
-            raise unittest.SkipTest('TestNWBContainerIO must be extended with setUpBuilder and setUpContainer implemented')
-        self.manager = get_build_manager()
-        self.setUpContainer()
-        self.setUpBuilder()
-
-    def test_build(self):
-        self.maxDiff = None
-        result = self.manager.build(self.container)
-        self.assertDictEqual(result, self.builder)
-
-    def test_construct(self):
-        result = self.manager.construct(self.builder)
-        self.assertContainerEqual(result, self.container)
-
-    def setUpBuilder(self):
-        ''' Should set the attribute 'builder' on self '''
-        if isinstance(self, TestNWBContainerIO):
-            raise unittest.SkipTest('Cannot run test unless setUpBuilder is implemented')
-
-    def setUpContainer(self):
-        ''' Should set the attribute 'container' on self '''
-        if isinstance(self, TestNWBContainerIO):
-            raise unittest.SkipTest('Cannot run test unless setUpContainer is implemented')
-
-    def assertContainerEqual(self, container1, container2):
-        type1 = type(container1)
-        type2 = type(container2)
-        self.assertEqual(type1, type2)
-        for nwbfield in container1.__nwbfields__:
-            with self.subTest(nwbfield=nwbfield, container_type=type1.__name__):
-                f1 = getattr(container1, nwbfield)
-                f2 = getattr(container2, nwbfield)
-                if isinstance(f1, tuple) or isinstance(f1, list):
-                    if len(f1) > 0 and isinstance(f1[0], NWBContainer):
-                        for sub1, sub2 in zip(f1,f2):
-                            self.assertContainerEqual(sub1, sub2)
-                        continue
-                self.assertEqual(f1, f2)
 
 class TestTimeSeriesIO(TestNWBContainerIO):
 
