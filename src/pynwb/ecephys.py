2d1e5717f51fc9d839aac11688343264c06e9316
Andrew Tritt <ajtritt@lbl.gov>
2017-05-17 22:09:50 -0700

    updating ecephys to correspond with spec

--- src/pynwb/ecephys.py
+++ src/pynwb/ecephys.py
@@ -7,84 +7,7 @@ from . import register_class, CORE_NAMESPACE
 from .base import TimeSeries, Interface, _default_resolution, _default_conversion
 from .core import NWBContainer
 
-@register_class('ElectricalSeries', CORE_NAMESPACE)
-class ElectricalSeries(TimeSeries):
-    """
-    Stores acquired voltage data from extracellular recordings. The data field of an ElectricalSeries
-    is an int or float array storing data in Volts. TimeSeries::data array structure: [num times] [num
-    channels] (or [num_times] for single electrode).
-    """
-
-    __nwbfields__ = ('electrode_group',)
-
-    __ancestry = "TimeSeries,ElectricalSeries"
-    __help = "Stores acquired voltage data from extracellular recordings."
-
-    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
-            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
-                                                   'contained here. It can also be the name of a device, for stimulus or '
-                                                   'acquisition data')},
-            {'name': 'data', 'type': (list, np.ndarray, TimeSeries), 'doc': 'The data this TimeSeries dataset stores. Can also store binary data e.g. image frames'},
-
-            {'name': 'electrode_group', 'type': (list, tuple, np.ndarray, DataChunkIterator), 'doc': 'The names of the electrode groups, or the ElectrodeGroup objects that each channel corresponds to.', 'ndim': 1},
-
-            {'name': 'resolution', 'type': float, 'doc': 'The smallest meaningful difference (in specified unit) between values in data', 'default': _default_resolution},
-            {'name': 'conversion', 'type': float, 'doc': 'Scalar to multiply each element by to conver to volts', 'default': _default_conversion},
-
-            {'name': 'timestamps', 'type': (list, np.ndarray, TimeSeries), 'doc': 'Timestamps for samples stored in data', 'default': None},
-            {'name': 'starting_time', 'type': float, 'doc': 'The timestamp of the first sample', 'default': None},
-            {'name': 'rate', 'type': float, 'doc': 'Sampling rate in Hz', 'default': None},
-
-            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
-            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
-            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
-            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
-            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
-    )
-    def __init__(self, **kwargs):
-        name, source, electrode_group, data = popargs('name', 'source', 'electrode_group', 'data', kwargs)
-        super(ElectricalSeries, self).__init__(name, source, data, 'volt', **kwargs)
-        self.electrode_group = tuple(electrode_group)
-
-
-@register_class('SpikeEventSeries', CORE_NAMESPACE)
-class SpikeEventSeries(ElectricalSeries):
-    """
-    Stores "snapshots" of spike events (i.e., threshold crossings) in data. This may also be raw data,
-    as reported by ephys hardware. If so, the TimeSeries::description field should describing how
-    events were detected. All SpikeEventSeries should reside in a module (under EventWaveform
-    interface) even if the spikes were reported and stored by hardware. All events span the same
-    recording channels and store snapshots of equal duration. TimeSeries::data array structure:
-    [num events] [num channels] [num samples] (or [num events] [num samples] for single
-    electrode).
-    """
-
-    __nwbfields__ = ()
-
-    __ancestry = "TimeSeries,ElectricalSeries,SpikeSeries"
-    __help = "Snapshots of spike events from data."
-
-    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
-            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
-                                                   'contained here. It can also be the name of a device, for stimulus or '
-                                                   'acquisition data')},
-            {'name': 'data', 'type': (list, np.ndarray, TimeSeries), 'doc': 'The data this TimeSeries dataset stores. Can also store binary data e.g. image frames'},
-            {'name': 'timestamps', 'type': (list, np.ndarray, TimeSeries), 'doc': 'Timestamps for samples stored in data'},
-            {'name': 'electrode_group', 'type': (list, tuple, np.ndarray, DataChunkIterator), 'doc': 'The names of the electrode groups, or the ElectrodeGroup objects that each channel corresponds to.', 'ndim': 1},
-
-            {'name': 'resolution', 'type': float, 'doc': 'The smallest meaningful difference (in specified unit) between values in data', 'default': _default_resolution},
-            {'name': 'conversion', 'type': float, 'doc': 'Scalar to multiply each element by to conver to volts', 'default': _default_conversion},
-
-            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
-            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
-            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
-            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
-            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
-    )
-    def __init__(self, **kwargs):
-        name, source, data, electrode_group = popargs('name', 'source', 'data', 'electrode_group', kwargs)
-        super(SpikeEventSeries, self).__init__(name, source, data, electrode_group, **kwargs)
-
+@register_class('Device', CORE_NAMESPACE)
 class Device(NWBContainer):
     """
     """
@@ -135,6 +58,84 @@ class ElectrodeGroup(NWBContainer):
         self.description = description
         self.location = location
         self.device = device
+
+@register_class('ElectricalSeries', CORE_NAMESPACE)
+class ElectricalSeries(TimeSeries):
+    """
+    Stores acquired voltage data from extracellular recordings. The data field of an ElectricalSeries
+    is an int or float array storing data in Volts. TimeSeries::data array structure: [num times] [num
+    channels] (or [num_times] for single electrode).
+    """
+
+    __nwbfields__ = ('electrode_group',)
+
+    __ancestry = "TimeSeries,ElectricalSeries"
+    __help = "Stores acquired voltage data from extracellular recordings."
+
+    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
+            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
+                                                   'contained here. It can also be the name of a device, for stimulus or '
+                                                   'acquisition data')},
+            {'name': 'data', 'type': (list, np.ndarray, TimeSeries), 'doc': 'The data this TimeSeries dataset stores. Can also store binary data e.g. image frames'},
+
+            {'name': 'electrode_group', 'type': ElectrodeGroup, 'doc': 'The names of the electrode groups, or the ElectrodeGroup objects that each channel corresponds to.'},
+
+            {'name': 'resolution', 'type': float, 'doc': 'The smallest meaningful difference (in specified unit) between values in data', 'default': _default_resolution},
+            {'name': 'conversion', 'type': float, 'doc': 'Scalar to multiply each element by to conver to volts', 'default': _default_conversion},
+
+            {'name': 'timestamps', 'type': (list, np.ndarray, TimeSeries), 'doc': 'Timestamps for samples stored in data', 'default': None},
+            {'name': 'starting_time', 'type': float, 'doc': 'The timestamp of the first sample', 'default': None},
+            {'name': 'rate', 'type': float, 'doc': 'Sampling rate in Hz', 'default': None},
+
+            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
+            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
+            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
+            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
+            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
+    )
+    def __init__(self, **kwargs):
+        name, source, electrode_group, data = popargs('name', 'source', 'electrode_group', 'data', kwargs)
+        super(ElectricalSeries, self).__init__(name, source, data, 'volt', **kwargs)
+        self.electrode_group = electrode_group
+
+
+@register_class('SpikeEventSeries', CORE_NAMESPACE)
+class SpikeEventSeries(ElectricalSeries):
+    """
+    Stores "snapshots" of spike events (i.e., threshold crossings) in data. This may also be raw data,
+    as reported by ephys hardware. If so, the TimeSeries::description field should describing how
+    events were detected. All SpikeEventSeries should reside in a module (under EventWaveform
+    interface) even if the spikes were reported and stored by hardware. All events span the same
+    recording channels and store snapshots of equal duration. TimeSeries::data array structure:
+    [num events] [num channels] [num samples] (or [num events] [num samples] for single
+    electrode).
+    """
+
+    __nwbfields__ = ()
+
+    __ancestry = "TimeSeries,ElectricalSeries,SpikeSeries"
+    __help = "Snapshots of spike events from data."
+
+    @docval({'name': 'name', 'type': str, 'doc': 'The name of this TimeSeries dataset'},
+            {'name': 'source', 'type': str, 'doc': ('Name of TimeSeries or Modules that serve as the source for the data '
+                                                   'contained here. It can also be the name of a device, for stimulus or '
+                                                   'acquisition data')},
+            {'name': 'data', 'type': (list, np.ndarray, TimeSeries), 'doc': 'The data this TimeSeries dataset stores. Can also store binary data e.g. image frames'},
+            {'name': 'timestamps', 'type': (list, np.ndarray, TimeSeries), 'doc': 'Timestamps for samples stored in data'},
+            {'name': 'electrode_group', 'type': ElectrodeGroup, 'doc': 'The names of the electrode groups, or the ElectrodeGroup objects that each channel corresponds to.', 'ndim': 1},
+
+            {'name': 'resolution', 'type': float, 'doc': 'The smallest meaningful difference (in specified unit) between values in data', 'default': _default_resolution},
+            {'name': 'conversion', 'type': float, 'doc': 'Scalar to multiply each element by to conver to volts', 'default': _default_conversion},
+
+            {'name': 'comments', 'type': str, 'doc': 'Human-readable comments about this TimeSeries dataset', 'default':None},
+            {'name': 'description', 'type': str, 'doc': 'Description of this TimeSeries dataset', 'default':None},
+            {'name': 'control', 'type': Iterable, 'doc': 'Numerical labels that apply to each element in data', 'default': None},
+            {'name': 'control_description', 'type': Iterable, 'doc': 'Description of each control value', 'default': None},
+            {'name': 'parent', 'type': 'NWBContainer', 'doc': 'The parent NWBContainer for this NWBContainer', 'default': None},
+    )
+    def __init__(self, **kwargs):
+        name, source, data, electrode_group = popargs('name', 'source', 'data', 'electrode_group', kwargs)
+        super(SpikeEventSeries, self).__init__(name, source, data, electrode_group, **kwargs)
 
 @register_class('EventDetection', CORE_NAMESPACE)
 class EventDetection(Interface):
