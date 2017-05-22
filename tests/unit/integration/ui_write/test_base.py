1c59b7a0cf2304c4910344d3f9bdc7483f96cce0
Andrew Tritt <ajtritt@lbl.gov>
2017-05-22 14:11:48 -0700

    added test to fail, just to see what it does to BB pipelines

--- tests/unit/integration/ui_write/test_base.py
+++ tests/unit/integration/ui_write/test_base.py
@@ -12,6 +12,9 @@ from . import base
 
 class TestTimeSeriesIO(base.TestNWBContainerIO):
 
+    def test_break(self):
+        self.assertEqual(False, True)
+
     def setUpContainer(self):
         self.container = TimeSeries('test_timeseries', 'example_source', list(range(100,200,10)), 'SIunit', timestamps=list(range(10)), resolution=0.1)
 
