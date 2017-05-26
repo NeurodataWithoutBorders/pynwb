2bb1076b17a47c9e30e18c6f9f4e149b45d5af6c
Oliver Ruebel <oruebel@lbl.gov>
2017-05-26 12:25:40 -0700

    Added fix for wide tables in RTD HTML theme and added missing folder for static html files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,6 +103,9 @@ pygments_style = 'sphinx'
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+def setup(app):
+   app.add_stylesheet("theme_overrides.css")  # overrides for wide tables in RTD theme
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
