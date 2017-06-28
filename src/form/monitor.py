e89d906892c1d80f50895b5a130101652f76d33e
Andrew Tritt <ajtritt@lbl.gov>
2017-06-27 21:49:42 -0700

    worked on class for monitoring a DataChunkIterator

--- src/form/monitor.py
+++ src/form/monitor.py
@@ -6,36 +6,37 @@ from .utils import DataChunkIterator
 class NotYetExhausted(Exception):
     pass
 
-class DataChunkProcessor(DataChunkIterator, metaclass=ABCMeta):
+class DataChunkProcessor(AbstractDataChunkIterator, metaclass=ABCMeta):
 
-
-    @docval({'name': 'data', 'type': None, 'doc': 'The data object used for iteration', 'default': None},
-            {'name': 'max_shape', 'type': tuple,
-             'doc': 'The maximum shape of the full data array. Use None to indicate unlimited dimensions',
-             'default': None},
-            {'name': 'dtype', 'type': np.dtype, 'doc': 'The Numpy data type for the array', 'default': None},
-            {'name': 'buffer_size', 'type': int, 'doc': 'Number of values to be buffered in a chunk', 'default': 1},
-            )
+    @docval({'name': 'data', 'type': DataChunkIterator, 'doc': 'the DataChunkIterator to analyze',})
     def __init__(self, **kwargs):
         """Initalize the DataChunkIterator"""
         # Get the user parameters
-        args, kwargs = fmt_docval_args(DataChunkIterator.__init__, kwargs)
-        super(DataChunkIterator, self).__init__(*args, **kwargs)
+        self.__dci = getargs('data', kwargs)
 
     def __next__(self):
         try:
-            dc = super(DataChunkIterator, self).__next__()
+            dc = self.__dci.__next__()
         except StopIteration as e:
             self.__done = True
             raise e
         self.process_data_chunk(dc)
+        return dc
+
+    def __iter__(self):
+        return iter(self.__dci)
+
+    def recommended_chunk_shape(self):
+        return self.__dci.recommended_chunk_shape()
+
+    def recommended_data_shape(self):
+        return self.__dci.recommended_data_shape()
 
     def get_final_result(self, **kwargs):
         ''' Return the result of processing data fed by this DataChunkIterator '''
         if not self.__done:
             raise NotYetExhausted()
         return self.compute_final_result()
-
 
     @abstractmethod
     @docval({'name': 'data_chunk', 'type': DataChunk, 'doc': 'a chunk to process'})
@@ -57,6 +58,7 @@ class NumSampleCounter(DataChunkProcessor):
 
     def __init__(self, **kwargs):
         args, kwargs = fmt_docval_args(DataChunkProcessor.__init__, kwargs)
+        super(NumSampleCounter, self).__init__(*args, **kwargs)
         self.__sample_count = 0
 
     @docval({'name': 'data_chunk', 'type': DataChunk, 'doc': 'a chunk to process'})
@@ -64,3 +66,6 @@ class NumSampleCounter(DataChunkProcessor):
         dc = getargs('data_chunk', kwargs)
         self.__sample_count += len(dc)
 
+    @docval(returns='the result of processing this stream')
+    def compute_final_result(self, **kwargs):
+        return self.__sample_count
