75cb34ec493834ba13d80c392ae899dec794ec34
Andrew Tritt <ajtritt@lbl.gov>
2017-09-07 14:59:13 -0700

    added ability to run tests verbosely or quietly

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 import unittest2 as unittest
 import sys
 
-result = unittest.TextTestRunner().run(unittest.defaultTestLoader.discover("tests/unit"))
+import argparse
+
+parser = argparse.ArgumentParser('python test.py [options]')
+parser.set_defaults(verbosity=1)
+parser.add_argument('-v', '--verbose', const=2, dest='verbosity', action='store_const', help='run in verbose mode')
+parser.add_argument('-q', '--quiet', const=0, dest='verbosity', action='store_const', help='run in verbose mode')
+
+args = parser.parse_args()
+
+result = unittest.TextTestRunner(verbosity=args.verbosity).run(unittest.defaultTestLoader.discover("tests/unit"))
 
 if result.errors or result.failures:
     sys.exit(1)
