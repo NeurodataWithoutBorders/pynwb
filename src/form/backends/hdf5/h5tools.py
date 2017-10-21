4b7154ddf57910595db68fc013d408fae055b77e
Andrew Tritt <ajtritt@lbl.gov>
2017-10-20 17:09:20 -0700

    moved H5RegionSlicer to its own submodule

--- src/form/backends/hdf5/h5tools.py
+++ src/form/backends/hdf5/h5tools.py
@@ -6,7 +6,6 @@ from six import raise_from, text_type, string_types, binary_type
 from functools import partial
 
 from form import Container
-from form.container import RegionSlicer
 from form.utils import docval, getargs, popargs
 from form.data_utils import DataChunkIterator, get_shape
 from form.build import Builder, GroupBuilder, DatasetBuilder, LinkBuilder, BuildManager, RegionBuilder
@@ -513,25 +512,4 @@ class HDF5IO(FORMIO):
             ret.append(tuple(new_item))
         return ret
 
-
-
-class H5RegionSlicer(RegionSlicer):
-
-    @docval({'name': 'dataset', 'type': Dataset, 'doc': 'the HDF5 dataset to slice'},
-            {'name': 'region', 'type': RegionReference, 'doc': 'the region reference to use to slice'})
-    def __init__(self, **kwargs):
-        self.__dataset = getargs('dataset', kwargs)
-        self.__regref = getargs('regref', kwargs)
-        self.__len = self.__dataset.regionref.selection(self.__regref)[0]
-
-    def __read_region(self):
-        if self.__region is None:
-            self.__region = self.__dataset[self.__regref]
-
-    def __getitem__(self, idx):
-        self.__read_region()
-        return self.__region[idx]
-
-    def __len__(self):
-        return self.__len
 
