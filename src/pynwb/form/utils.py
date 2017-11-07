7754cd5f9bfc529f36d889bd6903bbbe0957d913
Andrew Tritt <ajtritt@lbl.gov>
2017-11-07 15:21:25 -0800

    fixed bug when showing default strings in signature

--- src/pynwb/form/utils.py
+++ src/pynwb/form/utils.py
@@ -306,6 +306,17 @@ def docval(*validator, **options):
         return func_call
     return dec
 
+def __sig_arg(argval):
+    if 'default' in argval:
+        default = argval['default']
+        if isinstance(default, str):
+            default = "'%s'" % default
+        else:
+            default = str(default)
+        return "%s=%s" % (argval['name'], default)
+    else:
+        return argval['name']
+
 def __builddoc(func, validator, docstring_fmt, arg_fmt, ret_fmt=None, returns=None, rtype=None):
     '''Generate a Spinxy docstring'''
     def to_str(argtype):
@@ -323,12 +334,6 @@ def __builddoc(func, validator, docstring_fmt, arg_fmt, ret_fmt=None, returns=No
             fmt['type'] = to_str(arg['type'])
 
         return arg_fmt.format(**fmt)
-
-    def __sig_arg(argval):
-        if 'default' in argval:
-            return "%s=%s" % (argval['name'], str(argval['default']))
-        else:
-            return argval['name']
 
     sig =  "%s(%s)\n\n" % (func.__name__, ", ".join(map(__sig_arg, validator)))
     desc = func.__doc__.strip() if func.__doc__ is not None else ""
