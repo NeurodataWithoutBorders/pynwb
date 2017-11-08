8c077bb76f6284dddce65953fe3fd73d22a9e91f
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-11-08 09:23:43 -0500

    cli: Refactor code and fix invocation of validate command line tool

--- src/pynwb/validate.py
+++ src/pynwb/validate.py
@@ -1,28 +1,31 @@
 from __future__ import print_function
 
+import os
 import sys
 
+from argparse import ArgumentParser
 
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    import os
-    from form.backends.hdf5 import HDF5IO
-    from pynwb import validate, load_namespaces, get_manager
+from pynwb.form.backends.hdf5 import HDF5IO
+from pynwb import validate, load_namespaces, get_manager
+
+
+def _print_errors(validation_errors):
+    if validation_errors:
+        print(' - found the following errors:', file=sys.stderr)
+        for err in validation_errors:
+            print('%s - %s' % (err.name, err.reason), file=sys.stderr)
+    else:
+        print(' - no errors found.')
+
+
+def main():
 
     ep = """
     use --nspath to validate against an extension. If --ns is not specified,
     validate against all namespaces in namespace file.
     """
 
-    def print_errors(errors):
-        if len(errors) > 0:
-            print(' - found the following errors:', file=sys.stderr)
-            for err in errors:
-                print('%s - %s' % (err.name, err.reason), file=sys.stderr)
-        else:
-            print(' - no errors found.')
-
-    parser = ArgumentParser(description="Validate an NWB file")
+    parser = ArgumentParser(description="Validate an NWB file", epilog=ep)
     parser.add_argument("path", type=str, help="the path to the NWB file")
     parser.add_argument('-p', '--nspath', type=str, help="the path to the namespace file")
     parser.add_argument("-n", "--ns", type=str, help="the namespace to validate against")
@@ -44,8 +47,12 @@ if __name__ == '__main__':
             for ns in namespaces:
                 print('Validating against %s' % ns)
                 errors = validate(io, ns)
-                print_errors(errors)
+                _print_errors(errors)
     else:
         errors = validate(io)
-        print('Validating against core namespace' % ns)
-        print_errors(errors)
+        print('Validating against core namespace' % args.ns)
+        _print_errors(errors)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()
