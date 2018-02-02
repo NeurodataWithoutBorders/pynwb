d0e26da38b62b53203d6f2ec8a02914fff5f0288
Andrew Tritt <ajtritt@lbl.gov>
2018-02-01 21:56:41 -0800

    add ability to override default constructor of MultiContainerInterface

--- src/pynwb/core.py
+++ src/pynwb/core.py
@@ -393,7 +393,8 @@ class MultiContainerInterface(NWBDataInterface):
             doc = "a dictionary containing the %s in this %s container" % (container_type.__name__, cls.__name__)
             setattr(cls, attr, property(getter, cls._setter(attr), None, doc))
         setattr(cls, add, cls.__make_add(add, attr, container_type))
-        setattr(cls, '__init__', cls.__make_constructor(attr, add, container_type))
+        if cls.__init__ is MultiContainerInterface.__init__:
+            setattr(cls, '__init__', cls.__make_constructor(attr, add, container_type))
 
         # get create method name
         create = cls.__clsconf__.get('create')
