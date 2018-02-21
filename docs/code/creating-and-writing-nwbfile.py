c7935e09b4d2e55e020a0d52ae87793cb5e310b3
Ben Dichter <ben.dichter@gmail.com>
2018-02-21 15:19:46 -0800

    update file write demp
    add documentation for writing using a context, and use NWBHDF5IO instead of HDF5IO

--- docs/code/creating-and-writing-nwbfile.py
+++ docs/code/creating-and-writing-nwbfile.py
@@ -28,11 +28,15 @@ def main():
     # create-nwbfile: end
 
     # save-nwbfile: start
-    from pynwb import get_manager
-    from pynwb.form.backends.hdf5 import HDF5IO
-
+    from pynwb import NWBHDF5IO
+    
+    with NWBHDF5IO(filename, mode='w') as io:
+        io.write(f)
+        
+    # or
+   
     filename = "example.h5"
-    io = HDF5IO(filename, manager=get_manager(), mode='w')
+    io = NWBHDF5IO(filename, mode='w')
     io.write(f)
     io.close()
     # save-nwbfile: end
