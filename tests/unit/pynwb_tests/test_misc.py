af39796c6ab291748561c8be2c12335e8087dd8c
Andrew Tritt <ajtritt@lbl.gov>
2018-02-27 00:19:37 -0800

    fix pep8

--- tests/unit/pynwb_tests/test_misc.py
+++ tests/unit/pynwb_tests/test_misc.py
@@ -51,21 +51,22 @@ class UnitTimesConstructor(unittest.TestCase):
         ut = UnitTimes('UnitTimes constructor unit test')
         self.assertEqual(ut.source, 'UnitTimes constructor unit test')
         self.assertEqual(ut.name, 'UnitTimes')
-        self.assertEqual(ut.unit_ids, list())
-        self.assertEqual(ut.spike_times, list())
-        self.assertEqual(ut.spike_times_idx, list())
+        self.assertEqual(ut.unit_ids.data, list())
+        self.assertEqual(ut.spike_times.data, list())
+        self.assertEqual(ut.spike_times_index.data, list())
 
     def test_add_spike_times(self):
         ut = UnitTimes('UnitTimes add_spike_times unit test')
         ut.add_spike_times(0, [0, 1, 2])
         ut.add_spike_times(1, [3, 4, 5])
-        self.assertEqual(ut.unit_ids, [0, 1])
-        self.assertEqual(ut.spike_times, [0, 1, 2, 3, 4, 5])
-        self.assertEqual(len(ut.spike_times_idx), 2)
-        self.assertEqual(ut.spike_times_idx[0].target, [0, 1, 2, 3, 4, 5])
-        self.assertEqual(ut.spike_times_idx[0].slice, slice(0,3))
-        self.assertEqual(ut.spike_times_idx[1].target, [0, 1, 2, 3, 4, 5])
-        self.assertEqual(ut.spike_times_idx[1].slice, slice(3,6))
+        self.assertEqual(ut.unit_ids.data, [0, 1])
+        self.assertEqual(ut.spike_times.data, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(len(ut.spike_times_index), 2)
+        self.assertEqual(ut.spike_times_index[0].target.data, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(ut.spike_times_index[0].slice, slice(0, 3))
+        self.assertEqual(ut.spike_times_index[1].target.data, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(ut.spike_times_index[1].slice, slice(3, 6))
+
 
 if __name__ == '__main__':
     unittest.main()
