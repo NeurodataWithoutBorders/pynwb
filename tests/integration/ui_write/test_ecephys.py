b1e1cefa92513c5fe1def43dab3325bea7e475fe
Andrew Tritt <ajtritt@lbl.gov>
2018-03-06 17:22:02 -0800

    update ElectricalSeries integration test to check for resolution of ElectrodeGroup references in ElectrodeTableRegion

--- tests/integration/ui_write/test_ecephys.py
+++ tests/integration/ui_write/test_ecephys.py
@@ -89,6 +89,7 @@ class TestElectrodeGroupIO(base.TestMapRoundTrip):
 
 class TestElectricalSeriesIO(base.TestDataInterfaceIO):
 
+    @staticmethod
     def make_electrode_table(self):
         self.table = ElectrodeTable('electrodes')  # noqa: F405
         self.dev1 = Device('dev1', 'a test source')  # noqa: F405
@@ -99,6 +100,7 @@ class TestElectricalSeriesIO(base.TestDataInterfaceIO):
         self.table.add_row(3, 1.0, 2.0, 3.0, -3.0, 'CA1', 'none', 'third channel of tetrode', self.group)
         self.table.add_row(4, 1.0, 2.0, 3.0, -4.0, 'CA1', 'none', 'fourth channel of tetrode', self.group)
 
+    @staticmethod
     def get_table_builder(self):
         return DatasetBuilder('electrodes', self.table.data,
                               attributes={'neurodata_type': 'ElectrodeTable',
@@ -106,7 +108,7 @@ class TestElectricalSeriesIO(base.TestDataInterfaceIO):
                                           'help': 'a table for storing data about extracellular electrodes'})
 
     def setUpContainer(self):
-        self.make_electrode_table()
+        self.make_electrode_table(self)
         region = ElectrodeTableRegion(self.table, [0, 2], 'the first and third electrodes')  # noqa: F405
         data = list(zip(range(10), range(10, 20)))
         timestamps = list(map(lambda x: x/10, range(10)))
@@ -114,7 +116,7 @@ class TestElectricalSeriesIO(base.TestDataInterfaceIO):
         return ret
 
     def setUpBuilder(self):
-        table_builder = self.get_table_builder()
+        table_builder = self.get_table_builder(self)
         data = list(zip(range(10), range(10, 20)))
         timestamps = list(map(lambda x: x/10, range(10)))
         return GroupBuilder('test_eS',
@@ -148,13 +150,15 @@ class TestElectricalSeriesIO(base.TestDataInterfaceIO):
 
     def test_eg_ref(self):
         read = self.roundtripContainer()
-        import pdb
-        pdb.set_trace()
+        row1= read.electrodes[0]
+        row2= read.electrodes[1]
+        self.assertIsInstance(row1['group_ref'], ElectrodeGroup)
+        self.assertIsInstance(row2['group_ref'], ElectrodeGroup)
 
-class TestMultiElectricalSeries(TestElectricalSeriesIO):
+class TestMultiElectricalSeries(base.TestDataInterfaceIO):
 
     def setUpElectricalSeriesContainers(self):
-        self.make_electrode_table()
+        TestElectricalSeriesIO.make_electrode_table(self)
         region1 = ElectrodeTableRegion(self.table, [0, 2], 'the first and third electrodes')  # noqa: F405
         region2 = ElectrodeTableRegion(self.table, [1, 3], 'the second and fourth electrodes')  # noqa: F405
         data1 = list(zip(range(10), range(10, 20)))
@@ -165,7 +169,7 @@ class TestMultiElectricalSeries(TestElectricalSeriesIO):
         return (es1, es2)
 
     def setUpElectricalSeriesBuilders(self):
-        table_builder = self.get_table_builder()
+        table_builder = TestElectricalSeriesIO.get_table_builder(self)
         data = list(zip(range(10), range(10, 20)))
         timestamps = list(map(lambda x: x/10, range(10)))
         es1 = GroupBuilder('test_eS1',
@@ -212,6 +216,14 @@ class TestMultiElectricalSeries(TestElectricalSeriesIO):
                                                                       'description': 'the second and fourth electrodes',
                                                                       'help': 'a subset (i.e. slice or region) of an ElectrodeTable'})})  # noqa: E501
         return (es1, es2)
+
+    def addContainer(self, nwbfile):
+        ''' Should take an NWBFile object and add the container to it '''
+        nwbfile.add_device(self.dev1)
+        nwbfile.add_electrode_group(self.group)
+        nwbfile.set_electrode_table(self.table)
+        nwbfile.add_acquisition(self.container)
+
 
     def setUpContainer(self):
         raise unittest.SkipTest('Cannot run test unless addContainer is implemented')
