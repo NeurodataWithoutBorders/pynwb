ca165ceca6d46edf0bc3237de01121ad1830a14a
Andrew Tritt <ajtritt@lbl.gov>
2018-03-07 23:28:21 -0800

    dont make roi_list

--- src/pynwb/io/ophys.py
+++ src/pynwb/io/ophys.py
@@ -10,8 +10,6 @@ class PlaneSegmentationMap(ObjectMapper):
     # This might be needed for 2.0 as well
     def __init__(self, spec):
         super(PlaneSegmentationMap, self).__init__(spec)
-        roi_spec = self.spec.get_neurodata_type('ROI')
-        self.map_spec('roi_list', roi_spec)
 
         reference_images_spec = self.spec.get_group('reference_images').get_neurodata_type('ImageSeries')
         self.map_spec('reference_images', reference_images_spec)
