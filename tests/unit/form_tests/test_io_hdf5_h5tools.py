a74a854a588af8a5af0a2ab19de77720f8bade21
Oliver Ruebel <oruebel@lbl.gov>
2018-03-20 14:06:40 -0700

    Updated tests of compression option warning

--- tests/unit/form_tests/test_io_hdf5_h5tools.py
+++ tests/unit/form_tests/test_io_hdf5_h5tools.py
@@ -240,25 +240,29 @@ class H5IOTest(unittest.TestCase):
         self.assertEqual(dset.shuffle, True)
         self.assertEqual(dset.fletcher32, True)
 
-   #############################################
+    #############################################
     #  H5DataIO general
     #############################################
     def test_warning_on_non_gzip_compression(self):
         # Make sure no warning is issued when using gzip
         with warnings.catch_warnings(record=True) as w:
-            a = H5DataIO(np.arange(30),
-                         compression='gzip')
+            dset = H5DataIO(np.arange(30),
+                            compression='gzip')
             self.assertEqual(len(w), 0)
-        # Make sure no warning is issued when using lzf
-        with warnings.catch_warnings(record=True) as w:
-            a = H5DataIO(np.arange(30),
-                         compression='szip')
-            self.assertEqual(len(w), 1)
+            self.assertEqual(dset.io_settings['compression'], 'gzip')
         # Make sure no warning is issued when using szip
         with warnings.catch_warnings(record=True) as w:
-            a = H5DataIO(np.arange(30),
-                         compression='szip')
+            dset = H5DataIO(np.arange(30),
+                            compression='szip')
             self.assertEqual(len(w), 1)
+            self.assertEqual(dset.io_settings['compression'], 'szip')
+        # Make sure no warning is issued when using lzf
+        with warnings.catch_warnings(record=True) as w:
+            dset = H5DataIO(np.arange(30),
+                            compression='lzf')
+            self.assertEqual(len(w), 1)
+            self.assertEqual(dset.io_settings['compression'], 'lzf')
+
 
 if __name__ == '__main__':
     unittest.main()
