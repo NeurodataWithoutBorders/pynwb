4cfa48864e931b34c11ef333232f4aaa6ccd4eb5
Thomas Braun <thomas.braun@byte-physics.de>
2018-04-05 17:38:22 -0700

    TestMapNWBContainer: Fix assertDictEqual for nan values in the dictionary
    Having dictionaries with NaN entries results in assertDictEqual saying
    that the two dictionaries are not equal as NaN == NaN is False.
    
    We workaround that issue by replacing known entries if they are NaN with
    a constant.

--- tests/integration/ui_write/base.py
+++ tests/integration/ui_write/base.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import os
 import difflib
 import pprint
+import collections
 from unittest2.util import _common_shorten_repr
 import numpy as np
 
@@ -40,16 +41,38 @@ class TestMapNWBContainer(unittest.TestCase):
         return self.__manager
 
     def assertDictEqual(self, d1, d2, msg=None):
+        """
+        Custom dictionary comparison routine treating NaN == NaN as True
+        """
         self.assertIsInstance(d1, dict, 'First argument is not a dictionary')
         self.assertIsInstance(d2, dict, 'Second argument is not a dictionary')
 
         if d1 != d2:
-            standardMsg = '%s != %s' % _common_shorten_repr(d1, d2)
-            diff = ('\n' + '\n'.join(difflib.ndiff(
-                           pprint.pformat(d1).splitlines(),
-                           pprint.pformat(d2).splitlines())))
-            standardMsg = self._truncateMessage(standardMsg, diff)
-            self.fail(self._formatMessage(msg, standardMsg))
+
+            def update(d, replacement):
+                """
+                Recursively update all dictionary entries which are keys in
+                replacement and who's values is NaN
+                """
+                for k, v in d.items():
+                    if isinstance(v, collections.Mapping):
+                        update(v, replacement)
+
+                    if replacement.get(k) is not None and np.isnan(v):
+                            d[k] = replacement[k]
+
+            replacement = {}
+            replacement['resolution'] = 3.1415
+            update(d1, replacement)
+            update(d2, replacement)
+
+            if d1 != d2:
+                standardMsg = '%s != %s' % _common_shorten_repr(d1, d2)
+                diff = ('\n' + '\n'.join(difflib.ndiff(
+                               pprint.pformat(d1).splitlines(),
+                               pprint.pformat(d2).splitlines())))
+                standardMsg = self._truncateMessage(standardMsg, diff)
+                self.fail(self._formatMessage(msg, standardMsg))
 
     def test_build(self):
         try:
