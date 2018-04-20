a4701535997e822ecb34276c70bd8a8f8593a32a
Oliver Ruebel <oruebel@lbl.gov>
2018-04-20 14:52:29 -0700

    Satisfu flake 8

--- docs/gallery/general/iterative_write.py
+++ docs/gallery/general/iterative_write.py
@@ -159,8 +159,8 @@ def iter_sin(chunk_length=10, max_chunks=100):
     random samples of sin([0, 2pi]).
     """
     x = 0
-    num_chunks=0
-    while(x < 0.5 and num_chunks<max_chunks):
+    num_chunks = 0
+    while(x < 0.5 and num_chunks < max_chunks):
         val = np.asarray([sin(random() * 2 * pi) for i in range(chunk_length)])
         x = random()
         num_chunks += 1
