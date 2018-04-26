506a305e152796afecbc06f0eeac1a1f9abc8d4f
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-04-26 15:28:32 -0400

    Fix syntax errors for python3 but not style errors

--- src/pynwb/io/base.py
+++ src/pynwb/io/base.py
@@ -3,7 +3,7 @@ from .. import register_map
 
 from ..base import TimeSeries, ProcessingModule
 
-print "I won't work with python 3"
+print ("I won't work with python 3")
 
 
 @register_map(ProcessingModule)
