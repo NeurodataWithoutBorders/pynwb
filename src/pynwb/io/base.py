17e76258a6f511365155a55b85be58b9a79435a6
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-04-26 15:18:20 -0400

    Add a failing statement so that python3 tests fail but python2 passes

--- src/pynwb/io/base.py
+++ src/pynwb/io/base.py
@@ -3,6 +3,8 @@ from .. import register_map
 
 from ..base import TimeSeries, ProcessingModule
 
+print "I won't work with python 3"
+
 
 @register_map(ProcessingModule)
 class ModuleMap(ObjectMapper):
