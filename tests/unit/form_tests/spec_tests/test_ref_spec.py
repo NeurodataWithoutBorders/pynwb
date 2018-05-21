2ec73eb11a152a0f242ff4ec60bf3b0420441afc
Oliver Ruebel <oruebel@lbl.gov>
2018-05-21 13:53:25 -0700

    Added NWBRefSpec class and tests. Fix #332

---
+++ tests/unit/form_tests/spec_tests/test_ref_spec.py
@@ -0,0 +1,17 @@
+import unittest2 as unittest
+import json
+
+from pynwb.form.spec import RefSpec
+
+
+class RefSpecTests(unittest.TestCase):
+
+    def test_constructor(self):
+        spec = RefSpec('TimeSeries', 'object')
+        self.assertEqual(spec.target_type, 'TimeSeries')
+        self.assertEqual(spec.reftype, 'object')
+        json.dumps(spec)  # to ensure there are no circular links
+
+    def test_wrong_reference_type(self):
+        with self.assertRaises(ValueError):
+            RefSpec('TimeSeries', 'unknownreftype')
