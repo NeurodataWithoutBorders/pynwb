ad07b8469848d3c4aeeed1b6878a987a9679124d
Oliver Ruebel <oruebel@lbl.gov>
2018-05-29 15:53:30 -0700

    Added tests for the data type validation

--- tests/unit/form_tests/spec_tests/test_attribute_spec.py
+++ tests/unit/form_tests/spec_tests/test_attribute_spec.py
@@ -9,9 +9,9 @@ class AttributeSpecTests(unittest.TestCase):
     def test_constructor(self):
         spec = AttributeSpec('attribute1',
                              'my first attribute',
-                             'str')
+                             'text')
         self.assertEqual(spec['name'], 'attribute1')
-        self.assertEqual(spec['dtype'], 'str')
+        self.assertEqual(spec['dtype'], 'text')
         self.assertEqual(spec['doc'], 'my first attribute')
         self.assertIsNone(spec.parent)
         json.dumps(spec)  # to ensure there are no circular links
--- tests/unit/form_tests/spec_tests/test_dataset_spec.py
+++ tests/unit/form_tests/spec_tests/test_dataset_spec.py
@@ -1,7 +1,27 @@
 import unittest2 as unittest
 import json
 
-from pynwb.form.spec import GroupSpec, DatasetSpec, AttributeSpec, DtypeSpec
+from pynwb.form.spec import GroupSpec, DatasetSpec, AttributeSpec, DtypeSpec, DtypeHelper, RefSpec
+
+class DtypeSpecHelper(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_recommended_dtypes(self):
+        self.assertListEqual(DtypeHelper.recommended_primary_dtypes,
+                             list(DtypeHelper.primary_dtype_synonyms.keys()))
+
+    def test_valid_primary_dtypes(self):
+        a = set(list(DtypeHelper.primary_dtype_synonyms.keys()) + [vi for v in DtypeHelper.primary_dtype_synonyms.values()
+                                                               for vi in v] + ['number', ])
+        self.assertSetEqual(a, DtypeHelper.valid_primary_dtypes)
+
+    def test_simplify_cpd_type(self):
+        compound_type =  [DtypeSpec('test', 'test field', 'float'),
+                          DtypeSpec('test2', 'test field2', 'int')]
+        expected_result = ['float', 'int']
+        result = DtypeHelper.simplify_cpd_type(compound_type)
+        self.assertListEqual(result, expected_result)
 
 
 class DtypeSpecTests(unittest.TestCase):
@@ -24,8 +44,8 @@ class DtypeSpecTests(unittest.TestCase):
 class DatasetSpecTests(unittest.TestCase):
     def setUp(self):
         self.attributes = [
-            AttributeSpec('attribute1', 'my first attribute', 'str'),
-            AttributeSpec('attribute2', 'my second attribute', 'str')
+            AttributeSpec('attribute1', 'my first attribute', 'text'),
+            AttributeSpec('attribute2', 'my second attribute', 'text')
         ]
 
     def test_constructor(self):
@@ -60,6 +80,29 @@ class DatasetSpecTests(unittest.TestCase):
         self.assertListEqual(spec['attributes'], self.attributes)
         self.assertIs(spec, self.attributes[0].parent)
         self.assertIs(spec, self.attributes[1].parent)
+
+    def test_constructor_invalidate_dtype(self):
+        with self.assertRaises(ValueError):
+            DatasetSpec(doc='my first dataset',
+                        dtype='my bad dtype',     # <-- Expect AssertionError due to bad type
+                        name='dataset1',
+                        dimension=(None, None),
+                        attributes=self.attributes,
+                        linkable=False,
+                        namespace='core',
+                        data_type_def='EphysData')
+
+    def test_constructor_ref_spec(self):
+        dtype = RefSpec('TimeSeries', 'object')
+        spec = DatasetSpec(doc='my first dataset',
+                           dtype=dtype,
+                           name='dataset1',
+                           dimension=(None, None),
+                           attributes=self.attributes,
+                           linkable=False,
+                           namespace='core',
+                           data_type_def='EphysData')
+        self.assertDictEqual(spec['dtype'], dtype)
 
     def test_datatype_extension(self):
         base = DatasetSpec('my first dataset',
@@ -129,7 +172,7 @@ class DatasetSpecTests(unittest.TestCase):
                            data_type_def='SimpleTable')
         self.assertEqual(base['dtype'], [dtype1, dtype2])
         self.assertEqual(base['doc'], 'my first table')
-        dtype3 = DtypeSpec('column3', 'the third column', 'str')
+        dtype3 = DtypeSpec('column3', 'the third column', 'text')
         ext = DatasetSpec('my first table extension',
                           [dtype3],
                           namespace='core',
