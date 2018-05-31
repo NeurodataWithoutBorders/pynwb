6b8c2dc9dfb3c51bc412488d8def6fe0ccedfd44
Andrew Tritt <ajtritt@lbl.gov>
2018-05-30 17:11:19 -0700

    add more tests for TimeSeries

--- tests/unit/pynwb_tests/test_base.py
+++ tests/unit/pynwb_tests/test_base.py
@@ -1,6 +1,8 @@
 import unittest
 
 from pynwb.base import ProcessingModule, TimeSeries
+from pynwb.form.data_utils import DataChunkIterator
+from pynwb.form.backends.hdf5 import H5DataIO
 
 
 class TestProcessingModule(unittest.TestCase):
@@ -43,10 +45,38 @@ class TestTimeSeries(unittest.TestCase):
         ts2 = TimeSeries('test_ts2', 'unit test test_data_timeseries', ts1,
                          'grams', timestamps=[1.0, 1.1, 1.2, 1.3, 1.4, 1.5])
         self.assertEqual(ts2.data, [0, 1, 2, 3, 4, 5])
+        self.assertEqual(ts1.num_samples, ts2.num_samples)
 
     def test_timestamps_timeseries(self):
-        ts1 = TimeSeries('test_ts1', 'unit test test_data_timeseries', [0, 1, 2, 3, 4, 5],
+        ts1 = TimeSeries('test_ts1', 'unit test test_timestamps_timeseries', [0, 1, 2, 3, 4, 5],
                          'grams', timestamps=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
         ts2 = TimeSeries('test_ts2', 'unit test test_data_timeseries', [10, 11, 12, 13, 14, 15],
                          'grams', timestamps=ts1)
         self.assertEqual(ts2.timestamps, [0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
+
+    def test_nodata(self):
+        ts1 = TimeSeries('test_ts1', 'unit test test_DataIO', starting_time=0.0, rate=0.1)
+        self.assertEqual(ts1.num_samples, 0)
+
+    def test_dataio_list_data(self):
+        data = H5DataIO(list(range(100)))
+        ts1 = TimeSeries('test_ts1', 'unit test test_DataIO', data,
+                         'grams', starting_time=0.0, rate=0.1)
+        self.assertEqual(ts1.num_samples, 100)
+
+    def test_dataio_dci_data(self):
+        data = H5DataIO(DataChunkIterator(data=(i for i in range(100))))
+        ts1 = TimeSeries('test_ts1', 'unit test test_DataIO', data,
+                         'grams', starting_time=0.0, rate=0.1)
+        self.assertEqual(ts1.num_samples, -1)
+
+    def test_dci_data(self):
+        data = DataChunkIterator(data=(i for i in range(100)))
+        ts1 = TimeSeries('test_ts1', 'unit test test_DataIO', data,
+                         'grams', starting_time=0.0, rate=0.1)
+        self.assertEqual(ts1.num_samples, -1)
+
+    def test_no_time(self):
+        with self.assertRaisesRegex(TypeError, "either 'timestamps' or 'starting_time' and 'rate' must be specified"):
+            ts2 = TimeSeries('test_ts2', 'unit test test_data_timeseries', [10, 11, 12, 13, 14, 15], 'grams')
+
