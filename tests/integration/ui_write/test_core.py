07ef20a1065ef21a425f28a6b90c473e6d43ef54
Andrew Tritt <ajtritt@lbl.gov>
2018-06-26 10:51:52 -0700

    update tests

--- tests/integration/ui_write/test_core.py
+++ tests/integration/ui_write/test_core.py
@@ -11,10 +11,19 @@ class TestDynamicTableIO(base.TestMapRoundTrip):
         return DynamicTable('trials', 'DynamicTable integration test')
 
     def setUpBuilder(self):
+        id_builder = DatasetBuilder('id', data=[],
+                                    attributes={
+                                         'help': 'unique identifiers for a list of elements',
+                                         'namespace': 'core',
+                                         'neurodata_type': 'ElementIdentifiers',
+                                    })
         return GroupBuilder('trials',
                             attributes={
-                                 'help': 'A column-centric table',
-                                 'namespace': 'core',
-                                 'neurodata_type': 'DynamicTable',
-                                 'source': 'DynamicTable integration test',
-                            })
+                                'help': 'A column-centric table',
+                                'namespace': 'core',
+                                'neurodata_type': 'DynamicTable',
+                                'source': 'DynamicTable integration test',
+                                'colnames': tuple(),
+
+                            },
+                            datasets={'id': id_builder})
