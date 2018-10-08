47bb32f99a8c9cfbe2dbcc951cf3cf7789fa67bf
Andrew Tritt <ajtritt@lbl.gov>
2018-10-08 12:05:36 -0700

    remove group_name and fix docstring wrt description. Fix #657

--- src/pynwb/file.py
+++ src/pynwb/file.py
@@ -381,8 +381,6 @@ class NWBFile(MultiContainerInterface):
             {'name': 'location', 'type': str, 'doc': 'the location of electrode within the subject e.g. brain region'},
             {'name': 'filtering', 'type': str, 'doc': 'description of hardware filtering'},
             {'name': 'group', 'type': ElectrodeGroup, 'doc': 'the ElectrodeGroup object to add to this NWBFile'},
-            {'name': 'group_name', 'type': str, 'doc': 'the ElectrodeGroup object to add to this NWBFile',
-             'default': None},
             {'name': 'id', 'type': int, 'doc': 'a unique identifier for the electrode', 'default': None},
             allow_extra=True)
     def add_electrode(self, **kwargs):
@@ -391,7 +389,7 @@ class NWBFile(MultiContainerInterface):
         See :py:meth:`~pynwb.core.DynamicTable.add_row` for more details.
 
         Required fields are *x*, *y*, *z*, *imp*, *location*, *filtering*,
-        *description*, *group* and any columns that have been added
+        *group* and any columns that have been added
         (through calls to `add_electrode_columns`).
         """
         self.__check_electrodes()
