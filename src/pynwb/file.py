766bca3439845126405b22062b1edc1ed9b1db7f
bendichter <ben.dichter@gmail.com>
2018-11-01 15:16:56 -0400

    fix error when omitting tags

--- src/pynwb/file.py
+++ src/pynwb/file.py
@@ -384,7 +384,10 @@ class NWBFile(MultiContainerInterface):
         enclosure versus sleeping between explorations)
         """
         self.__check_epochs()
-        self.epoch_tags.update(kwargs.get('tags', list()))
+        if kwargs['tags'] is None:
+            self.epoch_tags.update(list())
+        else:
+            self.epoch_tags.update(kwargs['tags'])
         call_docval_func(self.epochs.add_interval, kwargs)
 
     def __check_electrodes(self):
