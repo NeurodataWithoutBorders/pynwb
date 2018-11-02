65346b8e362a7c8a9a97b898310cfa864d7befa9
Thomas Braun <thomas.braun@byte-physics.de>
2018-11-02 14:29:39 +0100

    Integration tests for icephys: Add also some for derived PatchClampSeries types

--- tests/integration/ui_write/test_icephys.py
+++ tests/integration/ui_write/test_icephys.py
@@ -1,4 +1,6 @@
-from pynwb.icephys import IntracellularElectrode, CurrentClampStimulusSeries
+from pynwb.icephys import (IntracellularElectrode, PatchClampSeries, CurrentClampStimulusSeries,
+                           VoltageClampStimulusSeries, CurrentClampSeries,
+                           VoltageClampSeries, IZeroClampSeries)
 from pynwb.device import Device
 
 from . import base
@@ -49,6 +51,12 @@ class TestPatchClampSeries(with_metaclass(ABCMeta, base.TestDataInterfaceIO)):
         nwbfile.add_device(self.device)
         super(TestPatchClampSeries, self).addContainer(nwbfile)
 
+    def setUpContainer(self):
+        self.setUpElectrode()
+        return PatchClampSeries(name="pcs", data=[1, 2, 3, 4, 5], unit='A',
+                                starting_time=123.6, rate=10e3, electrode=self.elec, gain=0.126,
+                                stimulus_description="gotcha ya!", sweep_number=4711)
+
 
 class TestCurrentClampStimulusSeries(TestPatchClampSeries):
 
@@ -56,3 +64,41 @@ class TestCurrentClampStimulusSeries(TestPatchClampSeries):
         self.setUpElectrode()
         return CurrentClampStimulusSeries(name="ccss", data=[1, 2, 3, 4, 5], unit='A',
                                           starting_time=123.6, rate=10e3, electrode=self.elec, gain=0.126)
+
+
+class TestVoltageClampStimulusSeries(TestPatchClampSeries):
+
+    def setUpContainer(self):
+        self.setUpElectrode()
+        return VoltageClampStimulusSeries(name="vcss", data=[1, 2, 3, 4, 5], unit='A',
+                                          starting_time=123.6, rate=10e3, electrode=self.elec, gain=0.126)
+
+
+class TestCurrentClampSeries(TestPatchClampSeries):
+
+    def setUpContainer(self):
+        self.setUpElectrode()
+        return CurrentClampSeries(name="ccs", data=[1, 2, 3, 4, 5], unit='A',
+                                  starting_time=123.6, rate=10e3, electrode=self.elec, gain=0.126,
+                                  bias_current=1.2, bridge_balance=2.3, capacitance_compensation=3.45)
+
+
+class TestVoltageClampSeries(TestPatchClampSeries):
+
+    def setUpContainer(self):
+        self.setUpElectrode()
+        return VoltageClampSeries(name="vcs", data=[1, 2, 3, 4, 5], unit='A',
+                                  starting_time=123.6, rate=10e3, electrode=self.elec,
+                                  gain=0.126, capacitance_fast=1.2, capacitance_slow=2.3,
+                                  resistance_comp_bandwidth=3.45, resistance_comp_correction=4.5,
+                                  resistance_comp_prediction=5.678, whole_cell_capacitance_comp=6.789,
+                                  whole_cell_series_resistance_comp=0.7)
+
+
+class TestIZeroClampSeries(TestPatchClampSeries):
+
+    def setUpContainer(self):
+        self.setUpElectrode()
+        return IZeroClampSeries(name="izcs", data=[1, 2, 3, 4, 5], unit='A',
+                                starting_time=123.6, rate=10e3, electrode=self.elec, gain=0.126,
+                                bias_current=1.2, bridge_balance=2.3, capacitance_compensation=3.45)
