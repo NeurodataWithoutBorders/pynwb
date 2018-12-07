1503576d7fec4d5ec590e96d029f740e056feecd
Eric Miller <angevineMiller@gmail.com>
2018-12-07 11:46:13 -0800

    WIP: integration tests for obs_intervals
    Integration tests for obs_intervals are in progress. Currently failing on TestUnitsIO.test_get_obs_intervals.
    It appears to be an array shape issue.

--- tests/integration/ui_write/test_misc.py
+++ tests/integration/ui_write/test_misc.py
@@ -10,8 +10,8 @@ class TestUnitsIO(base.TestDataInterfaceIO):
 
     def setUpContainer(self):
         ut = Units(name='UnitsTest', description='a simple table for testing Units')
-        ut.add_unit(spike_times=[0, 1, 2])
-        ut.add_unit(spike_times=[3, 4, 5])
+        ut.add_unit(spike_times=[0, 1, 2], obs_intervals=[(0, 1), (2, 3)])
+        ut.add_unit(spike_times=[3, 4, 5], obs_intervals=[(4, 5), (6, 7)])
         return ut
 
     def setUpBuilder(self):
@@ -30,15 +30,31 @@ class TestUnitsIO(base.TestDataInterfaceIO):
                                                  'namespace': 'core',
                                                  'target': ReferenceBuilder(st_builder),
                                                  'help': 'indexes into a list of values for a list of elements'})
+        
+        obs_builder = DatasetBuilder('obs_intervals', [(0, 1), (2, 3), (4, 5), (6, 7)],
+                                     attributes={'neurodata_type': 'VectorData',
+                                                 'namespace': 'core',
+                                                 'description': 'the observation intervals for each unit',
+                                                 'help': 'Values for a list of elements'})
+        
+        obsi_builder = DatasetBuilder('obs_intervals_index',
+                                      [2, 4],
+                                      attributes={'neurodata_type': 'VectorIndex',
+                                                 'namespace': 'core',
+                                                 'target': ReferenceBuilder(obs_builder),
+                                                 'help': 'indexes into a list of values for a list of elements'})
+        
         return GroupBuilder('UnitsTest',
                             attributes={'neurodata_type': 'Units',
                                         'namespace': 'core',
                                         'help': 'Data about spiking units',
                                         'description': 'a simple table for testing Units',
-                                        'colnames': ('spike_times',)},
+                                        'colnames': ('spike_times', 'obs_intervals',)},
                             datasets={'id': ids_builder,
                                       'spike_times': st_builder,
-                                      'spike_times_index': sti_builder})
+                                      'spike_times_index': sti_builder,
+                                      'obs_intervals': obs_builder,
+                                      'obs_intervals_index': obsi_builder})
 
     def test_get_spike_times(self):
         ut = self.roundtripContainer()
@@ -47,3 +63,11 @@ class TestUnitsIO(base.TestDataInterfaceIO):
         received = ut.get_unit_spike_times(1)
         self.assertTrue(np.array_equal(received, [3, 4, 5]))
         self.assertTrue(np.array_equal(ut['spike_times'][:], [[0, 1, 2], [3, 4, 5]]))
+        
+    def test_get_obs_intervals(self):
+        ut = self.roundtripContainer()
+        received = ut.get_unit_obs_intervals(0)
+        self.assertTrue(np.array_equal(received, [(0, 1), (2, 3)]))
+        received = ut.get_unit_obs_intervals(1)
+        self.assertTrue(np.array_equal(received, [(4, 5), (6, 7)]))
+        self.assertTrue(np.array_equal(ut['obs_intervals'][:], [[(0, 1), (2, 3)], [(4, 5), (6, 7)]]))
