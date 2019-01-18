fcb919a3b5c50a92c473e04a6a0b66f7e0204a27
Tom Davidson <tjd@alum.mit.edu>
2019-01-18 00:10:23 -0800

    flake8 fixes

--- docs/gallery/general/file.py
+++ docs/gallery/general/file.py
@@ -198,9 +198,12 @@ nwbfile.add_trial(start_time=6.0, stop_time=8.0, stim='desert')
 nwbfile.add_unit_column('location', 'the anatomical location of this unit')
 nwbfile.add_unit_column('quality', 'the quality for the inference of this unit')
 
-nwbfile.add_unit(id=1, spike_times=[2.2, 3.0, 4.5], obs_intervals=[[1, 10]], location='CA1', quality=0.95)
-nwbfile.add_unit(id=2, spike_times=[2.2, 3.0, 25.0, 26.0], obs_intervals=[[1, 10], [20, 30]], location='CA3', quality=0.85)
-nwbfile.add_unit(id=3, spike_times=[1.2, 2.3, 3.3, 4.5], obs_intervals=[[1, 10], [20, 30]], location='CA1', quality=0.90)
+nwbfile.add_unit(id=1, spike_times=[2.2, 3.0, 4.5],
+                 obs_intervals=[[1, 10]], location='CA1', quality=0.95)
+nwbfile.add_unit(id=2, spike_times=[2.2, 3.0, 25.0, 26.0],
+                 obs_intervals=[[1, 10], [20, 30]], location='CA3', quality=0.85)
+nwbfile.add_unit(id=3, spike_times=[1.2, 2.3, 3.3, 4.5],
+                 obs_intervals=[[1, 10], [20, 30]], location='CA1', quality=0.90)
 
 ####################
 # .. _units_fields_ref:
