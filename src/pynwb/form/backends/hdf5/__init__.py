aadb9db0b8224b226bb6ef8123ee79099eb17bca
Andrew Tritt <ajtritt@lbl.gov>
2019-03-08 14:59:08 -0800

    remove form subpackage

--- src/pynwb/form/backends/hdf5/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa: F401
-from . import h5_utils
-from .h5tools import HDF5IO
-from .h5_utils import H5RegionSlicer, H5DataIO
-from . import h5tools
-from .h5tools import H5SpecWriter
-from .h5tools import H5SpecReader
