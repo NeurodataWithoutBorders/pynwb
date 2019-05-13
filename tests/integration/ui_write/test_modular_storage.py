f7d216dfd678f0fc4b9dc2ffc2e21f8c8d414259
nile graddis <nilegraddis@gmail.com>
2019-05-13 10:18:22 -0400

    #907 basic test for roundtrip reading

--- tests/integration/ui_write/test_modular_storage.py
+++ tests/integration/ui_write/test_modular_storage.py
@@ -1,4 +1,7 @@
 import os
+import tempfile
+import shutil
+import unittest
 from datetime import datetime
 from dateutil.tz import tzutc
 
@@ -83,3 +86,81 @@ class TestTimeSeriesModular(base.TestMapRoundTrip):
 
     def addContainer(self, nwbfile):
         pass
+
+
+class TestMissingLink(base.TestMapNWBContainer):
+
+    _required_tests = ('test_roundtrip', 'test_missing')
+
+    @property
+    def data(self):
+        return np.arange(50)
+
+    @property
+    def timestamps(self):
+        return np.linspace(0, 1, 50)
+
+    def setUp(self):
+        self.dirname = tempfile.mkdtemp()
+        self.data_filename = os.path.join(self.dirname, 'data.nwb')
+        self.link_filename = os.path.join(self.dirname, 'link.nwb')
+
+        self.data_file = NWBFile(
+            session_description='a test file',
+            identifier='data_file',
+            session_start_time=datetime.now()
+        )
+        self.data_file.add_acquisition(self.timeseries())
+
+        with HDF5IO(self.data_filename, 'w', manager=get_manager()) as data_write_io:
+            data_write_io.write(self.data_file)
+
+        with HDF5IO(self.data_filename, 'r', manager=get_manager()) as data_read_io:
+            data_file = data_read_io.read()
+
+            with HDF5IO(self.link_filename, 'w', manager=get_manager()) as link_write_io:
+                link_file = NWBFile(
+                    session_description='a test file',
+                    identifier='link_file',
+                    session_start_time=datetime.now()
+                )
+                link_file.add_acquisition(TimeSeries(
+                    name='test_mod_ts',
+                    unit='V',
+                    data=data_file.get_acquisition('data_ts'),
+                    timestamps=H5DataIO(
+                        data=data_file.get_acquisition('data_ts').timestamps,
+                        link_data=True
+                    )
+                ))
+                link_write_io.write(link_file)
+
+    def timeseries(self):
+        return TimeSeries(
+            name='data_ts',
+            unit='V',
+            data=self.data,
+            timestamps=self.timestamps
+        )
+
+    def tearDown(self):
+        shutil.rmtree(self.dirname)
+
+    def test_roundtrip(self):
+        expected = self.timeseries()
+
+        with HDF5IO(self.link_filename, 'r', manager=get_manager()) as link_io:
+            read_fil = link_io.read()
+            obtained = read_fil.get_acquisition('test_mod_ts')
+
+            self.assertNotEqual(id(expected), id(obtained))
+            self.assertContainerEqual(expected, obtained)
+
+    def test_missing(self):
+        
+        tmp_data_filename = os.path.join(self.dirname, '_data.nwb')
+        shutil.move(self.data_filename, tmp_data_filename)
+
+        with HDF5IO(self.link_filename, 'r', manager=get_manager()) as link_io:
+            read_fil = link_io.read()
+            obtained = read_fil.get_acquisition('test_mod_ts')
