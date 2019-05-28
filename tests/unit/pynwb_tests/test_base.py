e88da1a99434e41423c5ea442d3c251e4982e46f
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2019-05-28 15:51:39 -0700

    add tests for deprecated tools

--- tests/unit/pynwb_tests/test_base.py
+++ tests/unit/pynwb_tests/test_base.py
@@ -29,18 +29,33 @@ class TestProcessingModule(unittest.TestCase):
         self.assertIn(ts.name, self.pm.containers)
         self.assertIs(ts, self.pm.containers[ts.name])
 
+    def test_deprecated_add_container(self):
+        ts = TimeSeries('test_ts', [0, 1, 2, 3, 4, 5],
+                        'grams', timestamps=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
+        self.pm.add_container(ts)
+        self.assertIn(ts.name, self.pm.containers)
+        self.assertIs(ts, self.pm.containers[ts.name])
+
     def test_get_data_interface(self):
         ts = TimeSeries('test_ts', [0, 1, 2, 3, 4, 5],
                         'grams', timestamps=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
         self.pm.add(ts)
         tmp = self.pm.get('test_ts')
         self.assertIs(tmp, ts)
+        self.assertIs(self.pm['test_ts'], self.pm.get('test_ts'))
 
     def test_deprecated_get_data_interface(self):
         ts = TimeSeries('test_ts', [0, 1, 2, 3, 4, 5],
                         'grams', timestamps=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
         self.pm.add(ts)
         tmp = self.pm.get_data_interface('test_ts')
+        self.assertIs(tmp, ts)
+
+    def test_deprecated_get_container(self):
+        ts = TimeSeries('test_ts', [0, 1, 2, 3, 4, 5],
+                        'grams', timestamps=[0.0, 0.1, 0.2, 0.3, 0.4, 0.5])
+        self.pm.add(ts)
+        tmp = self.pm.get_container('test_ts')
         self.assertIs(tmp, ts)
 
     def test_getitem(self):
