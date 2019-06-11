NeurodataWithoutBorders/pynwb

commit 6851c31fd2cdf074867479f36b227b5117e6644f

added module-level function for validating

src/pynwb/__init__.py
@@ -42,6 +42,7 @@ def load_namespaces(**kwargs):
     namespace_path = getargs('namespace_path', kwargs)
     __TYPE_MAP.load_namespaces(namespace_path)
 
+# load the core namespace i.e. base NWB specification
 __resources = __get_resources()
 if os.path.exists(__resources['namespace_path']):
     load_namespaces(__resources['namespace_path'])
@@ -56,6 +57,7 @@ def get_build_manager(**kwargs):
         type_map = __TYPE_MAP
     return __BuildManager(type_map)
 
+# a function to register a container classes with the global map
 @docval({'name': 'neurodata_type', 'type': str, 'doc': 'the neurodata_type to get the spec for'},
         {'name': 'namespace', 'type': str, 'doc': 'the name of the namespace'},
         {"name": "container_cls", "type": type, "doc": "the class to map to the specified neurodata_type", 'default': None},
@@ -74,6 +76,7 @@ def register_class(**kwargs):
     else:
         _dec(container_cls)
 
+# a function to register an object mapper for a container class
 @docval({"name": "container_cls", "type": type, "doc": "the Container class for which the given ObjectMapper class gets used for"},
         {"name": "mapper_cls", "type": type, "doc": "the ObjectMapper class to use to map", 'default': None},
         is_method=False)
@@ -91,6 +94,7 @@ def register_map(**kwargs):
     else:
         _dec(mapper_cls)
 
+# a function to get the container class for a give type
 @docval({'name': 'neurodata_type', 'type': str, 'doc': 'the neurodata_type to get the NWBConatiner class for'},
         {'name': 'namespace', 'type': str, 'doc': 'the namespace the neurodata_type is defined in'},
         is_method=False)
@@ -99,6 +103,17 @@ def get_class(**kwargs):
     """
     neurodata_type, namespace = getargs('neurodata_type', 'namespace', kwargs)
     return __TYPE_MAP.create_container_cls(namespace, neurodata_type)
+
+@docval({'name': 'io', 'type': FORMIO, 'doc': 'the FORMIO object to read from'},
+        {'name': 'namespace', 'type': str, 'doc': 'the namespace to validate against', 'default':CORE_NAMESPACE},
+        returns="errors in the file", rtype=list,
+        is_method=False)
+def validate(**kwargs):
+    """Validate an NWB file against a namespace"""
+    io, namespace = getargs('io', 'namespace', kwargs)
+    builder = io.read_builder()
+    validator = ValidatorMap(__NS_CATALOG.get_namespace(namespace)
+    return validator.validate(builder)
 
 
 from . import io as __io

commit 210b5a72a2f3cd6b461917f21ee4bd37fdce0f9e

validate: Use the namespace catalog of the passed HDF5IO object
Using the global namespace catalog here ignores a possibly different one
stored in the HDF5IO object.

Bug introduced in initial version in 6851c31f (added module-level
function for validating, 2017-07-28).

src/pynwb/__init__.py
@@ -181,7 +181,7 @@ def validate(**kwargs):
     """Validate an NWB file against a namespace"""
     io, namespace = getargs('io', 'namespace', kwargs)
     builder = io.read_builder()
-    validator = ValidatorMap(__NS_CATALOG.get_namespace(namespace))
+    validator = ValidatorMap(io.manager.namespace_catalog.get_namespace(name=namespace))
     return validator.validate(builder)
 
 