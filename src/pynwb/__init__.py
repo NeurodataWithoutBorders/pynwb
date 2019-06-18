NeurodataWithoutBorders/pynwb

commit 6851c31fd2cdf074867479f36b227b5117e6644f

added module-level function for validating

src/pynwb/__init__.py
@@ -42,6 +42,7 @@ def load_namespaces(**kwargs):
     namespace_path = getargs('namespace_path', kwargs)
     __TYPE_MAP.load_namespaces(namespace_path)
 
+# load the core namespace i.e. base NWB specification
 __resources = __get_resources()
 if os.path.exists(__resources['namespace_path']):
     load_namespaces(__resources['namespace_path'])
@@ -56,6 +57,7 @@ def get_build_manager(**kwargs):
         type_map = __TYPE_MAP
     return __BuildManager(type_map)
 
+# a function to register a container classes with the global map
 @docval({'name': 'neurodata_type', 'type': str, 'doc': 'the neurodata_type to get the spec for'},
         {'name': 'namespace', 'type': str, 'doc': 'the name of the namespace'},
         {"name": "container_cls", "type": type, "doc": "the class to map to the specified neurodata_type", 'default': None},
@@ -74,6 +76,7 @@ def register_class(**kwargs):
     else:
         _dec(container_cls)
 
+# a function to register an object mapper for a container class
 @docval({"name": "container_cls", "type": type, "doc": "the Container class for which the given ObjectMapper class gets used for"},
         {"name": "mapper_cls", "type": type, "doc": "the ObjectMapper class to use to map", 'default': None},
         is_method=False)
@@ -91,6 +94,7 @@ def register_map(**kwargs):
     else:
         _dec(mapper_cls)
 
+# a function to get the container class for a give type
 @docval({'name': 'neurodata_type', 'type': str, 'doc': 'the neurodata_type to get the NWBConatiner class for'},
         {'name': 'namespace', 'type': str, 'doc': 'the namespace the neurodata_type is defined in'},
         is_method=False)
@@ -99,6 +103,17 @@ def get_class(**kwargs):
     """
     neurodata_type, namespace = getargs('neurodata_type', 'namespace', kwargs)
     return __TYPE_MAP.create_container_cls(namespace, neurodata_type)
+
+@docval({'name': 'io', 'type': FORMIO, 'doc': 'the FORMIO object to read from'},
+        {'name': 'namespace', 'type': str, 'doc': 'the namespace to validate against', 'default':CORE_NAMESPACE},
+        returns="errors in the file", rtype=list,
+        is_method=False)
+def validate(**kwargs):
+    """Validate an NWB file against a namespace"""
+    io, namespace = getargs('io', 'namespace', kwargs)
+    builder = io.read_builder()
+    validator = ValidatorMap(__NS_CATALOG.get_namespace(namespace)
+    return validator.validate(builder)
 
 
 from . import io as __io

commit eb92f04af9c6f23fa5ba23b44a21830387c74f9a

Allow to validate files against the cached spec (#944)
* validate: Use the namespace catalog of the passed HDF5IO object

Using the global namespace catalog here ignores a possibly different one
stored in the HDF5IO object.

Bug introduced in initial version in 6851c31f (added module-level
function for validating, 2017-07-28).

* pynwb.validate._validate_helper: Make the return value a boolean

We only want to return 0 (success) or 1 (failure) as command line
application.

Broken since ebb11abf (pynwb.validate: Rewrite and accept multiple
paths, 2019-01-14).

* pynwb.validate: Rewrite and enhance

The current code has the following deficiencies:
- --nspath argument does not work
- no error checking that a namespace is available in the loaded namespaces
- does not allow to check against a cached specification

The rewrite introduces the new options --cached-namespace (default)
which allows to check against a cached namespace information. The --no
option is added to override the default.

All possible combinations of the options are supported, for all others
we complain.

src/pynwb/__init__.py
@@ -181,7 +181,7 @@ def validate(**kwargs):
     """Validate an NWB file against a namespace"""
     io, namespace = getargs('io', 'namespace', kwargs)
     builder = io.read_builder()
-    validator = ValidatorMap(__NS_CATALOG.get_namespace(namespace))
+    validator = ValidatorMap(io.manager.namespace_catalog.get_namespace(name=namespace))
     return validator.validate(builder)
 
 

src/pynwb/validate.py
@@ -2,10 +2,15 @@ from __future__ import print_function
 
 import os
 import sys
-
 from argparse import ArgumentParser
 
-from pynwb import validate, load_namespaces, get_manager, NWBHDF5IO
+from hdmf.spec import NamespaceCatalog
+from hdmf.build import BuildManager
+from hdmf.build import TypeMap as TypeMap
+from hdmf.backends.hdf5 import HDF5IO
+
+from pynwb import validate, available_namespaces, NWBHDF5IO
+from pynwb.spec import NWBDatasetSpec, NWBGroupSpec, NWBNamespace
 
 
 def _print_errors(validation_errors):
@@ -21,7 +26,7 @@ def _validate_helper(**kwargs):
     errors = validate(**kwargs)
     _print_errors(errors)
 
-    return (errors and len(errors) > 0)
+    return (errors is not None and len(errors) > 0)
 
 
 def main():
@@ -33,34 +38,77 @@ def main():
 
     parser = ArgumentParser(description="Validate an NWB file", epilog=ep)
     parser.add_argument("paths", type=str, nargs='+', help="NWB file paths")
-    parser.add_argument('-p', '--nspath', type=str, help="the path to the namespace file")
+    parser.add_argument('-p', '--nspath', type=str, help="the path to the namespace YAML file")
     parser.add_argument("-n", "--ns", type=str, help="the namespace to validate against")
+
+    feature_parser = parser.add_mutually_exclusive_group(required=False)
+    feature_parser.add_argument("--cached-namespace", dest="cached_namespace", action='store_true',
+                                help="Use the cached namespace (default).")
+    feature_parser.add_argument('--no-cached-namespace', dest="cached_namespace", action='store_false',
+                                help="Don't use the cached namespace.")
+    parser.set_defaults(cached_namespace=True)
 
     args = parser.parse_args()
     ret = 0
 
+    if args.nspath:
+        if not os.path.isfile(args.nspath):
+            print("The namespace file {} is not a valid file.".format(args.nspath), file=sys.stderr)
+            sys.exit(1)
+
+        if args.cached_namespace:
+            print("Turning off validation against cached namespace information"
+                  "as --nspath was passed.", file=sys.stderr)
+            args.cached_namespace = False
+
     for path in args.paths:
 
-        if not os.path.exists(path):
-            print('%s not found' % path, file=sys.stderr)
+        if not os.path.isfile(path):
+            print("The file {} does not exist.".format(path), file=sys.stderr)
             ret = 1
             continue
 
-        with NWBHDF5IO(path, get_manager(), mode='r') as io:
-
-            if args.nspath is not None:
-                namespaces = load_namespaces(args.nspath)
-                if args.ns is not None:
-                    print('Validating %s against %s from %s.' % (path, args.ns, args.ns_path))
-                    ret = ret or _validate_helper(io=io, namespace=args.ns)
-                else:
-                    print('Validating %s using namespaces in %s.' % (path, args.nspath))
-                    for ns in namespaces:
-                        print('Validating against %s' % ns)
-                        ret = ret or _validate_helper(io=io, namespace=ns)
+        if args.cached_namespace:
+            catalog = NamespaceCatalog(NWBGroupSpec, NWBDatasetSpec, NWBNamespace)
+            namespaces = HDF5IO.load_namespaces(catalog, path).keys()
+            if len(namespaces) > 0:
+                tm = TypeMap(catalog)
+                manager = BuildManager(tm)
+                specloc = "cached namespace information"
             else:
-                print('Validating %s against core namespace' % path)
-                ret = ret or _validate_helper(io=io)
+                manager = None
+                namespaces = available_namespaces()
+                specloc = "pynwb namespace information"
+                print("The file {} has no cached namespace information. "
+                      "Falling back to {}.".format(path, specloc), file=sys.stderr)
+        elif args.nspath:
+            catalog = NamespaceCatalog(NWBGroupSpec, NWBDatasetSpec, NWBNamespace)
+            namespaces = catalog.load_namespaces(args.nspath)
+
+            if len(namespaces) == 0:
+                print("Could not load namespaces from file {}.".format(args.nspath), file=sys.stderr)
+                sys.exit(1)
+
+            tm = TypeMap(catalog)
+            manager = BuildManager(tm)
+            specloc = "--nspath namespace information"
+        else:
+            manager = None
+            namespaces = available_namespaces()
+            specloc = "pynwb namespace information"
+
+        if args.ns:
+            if args.ns in namespaces:
+                namespaces = [args.ns]
+            else:
+                print("The namespace {} could not be found in {}.".format(args.ns, specloc), file=sys.stderr)
+                ret = 1
+                continue
+
+        with NWBHDF5IO(path, mode='r', manager=manager) as io:
+            for ns in namespaces:
+                print("Validating {} against {} using namespace {}.".format(path, specloc, ns))
+                ret = ret or _validate_helper(io=io, namespace=ns)
 
     sys.exit(ret)
 