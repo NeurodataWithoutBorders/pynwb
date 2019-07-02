70e2006e10596e1c404dd0cd083f084ba6a8597d
Ryan Ly <rly@lbl.gov>
2019-07-02 10:50:06 -0700

    Add colored unit test runner

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ import os
 import sys
 import traceback
 import unittest2 as unittest
+from coloredtestrunner import ColoredTestRunner, ColoredTestResult
 
 flags = {'pynwb': 2, 'integration': 3, 'example': 4}
 
@@ -21,42 +22,21 @@ FAILURES = 0
 ERRORS = 0
 
 
-class SuccessRecordingResult(unittest.TextTestResult):
-    '''A unittest test result class that stores successful test cases as well
-    as failures and skips.
-    '''
-
-    def addSuccess(self, test):
-        if not hasattr(self, 'successes'):
-            self.successes = [test]
-        else:
-            self.successes.append(test)
-
-    def get_all_cases_run(self):
-        '''Return a list of each test case which failed or succeeded
-        '''
-
-        cases = []
-
-        if hasattr(self, 'successes'):
-            cases.extend(self.successes)
-        cases.extend([failure[0] for failure in self.failures])
-
-        return cases
-
-
 def run_test_suite(directory, description="", verbose=True):
     global TOTAL, FAILURES, ERRORS
     logging.info("running %s" % description)
     directory = os.path.join(os.path.dirname(__file__), directory)
-    runner = unittest.TextTestRunner(verbosity=verbose, resultclass=SuccessRecordingResult)
-    pynwb_test_result = runner.run(unittest.TestLoader().discover(directory))
+    if verbose > 1:
+        runner = ColoredTestRunner(verbosity=verbose)
+    else:
+        runner = unittest.TextTestRunner(verbosity=verbose, resultclass=ColoredTestResult)
+    test_result = runner.run(unittest.TestLoader().discover(directory))
 
-    TOTAL += pynwb_test_result.testsRun
-    FAILURES += len(pynwb_test_result.failures)
-    ERRORS += len(pynwb_test_result.errors)
+    TOTAL += test_result.testsRun
+    FAILURES += len(test_result.failures)
+    ERRORS += len(test_result.errors)
 
-    return pynwb_test_result
+    return test_result
 
 
 def _import_from_file(script):
