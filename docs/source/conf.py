NeurodataWithoutBorders/pynwb

commit fe3d760b2d81189e877739eb0982b12296b6149d

add skip to ensure __getitem__ gets documented

docs/source/conf.py
@@ -288,8 +288,16 @@ class PatchedPythonDomain(PythonDomain):
         return super(PatchedPythonDomain, self).resolve_xref(
             env, fromdocname, builder, typ, target, node, contnode)
 
+from abc import abstractmethod, abstractproperty
+def skip(app, what, name, obj, skip, options):
+    if isinstance(obj, abstractproperty) or getattr(obj, '__isabstractmethod__', False):
+        return False
+    elif name == "__getitem__":
+        return False
+    return skip
 
 def setup(app):
     app.connect('builder-inited', run_apidoc)
     app.add_stylesheet("theme_overrides.css")  # overrides for wide tables in RTD theme
     app.override_domain(PatchedPythonDomain)
+    app.connect("autodoc-skip-member", skip)

commit bf9e63d1c0257d459e5ea16785b5a6d97b60ad51

Documentation: Make conf.py flake8 clean
Bug introduced in fe3d760b (add skip to ensure __getitem__ gets
documented, 2018-02-09).

docs/source/conf.py
@@ -325,7 +325,8 @@ class PatchedPythonDomain(PythonDomain):
             env, fromdocname, builder, typ, target, node, contnode)
 
 
-from abc import abstractmethod, abstractproperty
+from abc import abstractproperty
+
 
 def skip(app, what, name, obj, skip, options):
     if isinstance(obj, abstractproperty) or getattr(obj, '__isabstractmethod__', False):