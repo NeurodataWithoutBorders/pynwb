c905a26d5b72188ae580eef61f0af74b5469f656
Andrew Tritt <ajtritt@lbl.gov>
2019-10-24 15:04:03 -0700

    Update src/pynwb/misc.py
    Co-Authored-By: Ben Dichter <ben.dichter@gmail.com>

--- src/pynwb/misc.py
+++ src/pynwb/misc.py
@@ -187,7 +187,7 @@ class Units(DynamicTable):
                     nwbfile = self.get_ancestor(data_type='NWBFile')
                     elec_col.table = nwbfile.electrodes
                     if elec_col.table is None:
-                        warnings.warn('Reference to electrode table that does not yet exit')
+                        warnings.warn('Reference to electrode table that does not yet exist')
                 else:
                     elec_col.table = self.__electrode_table
 
