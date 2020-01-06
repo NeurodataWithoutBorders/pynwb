fe335739179bd8c11ec820defbb2816164b84a1e
Ryan Ly <rly@lbl.gov>
2020-01-06 17:25:25 -0500

    Update legal (#1131)
    * Update copyright year to 2020
    
    * Add Legal.txt to source distribution

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,7 +95,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'PyNWB'
-copyright = u'2017-2019, Neurodata Without Borders'
+copyright = u'2017-2020, Neurodata Without Borders'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
