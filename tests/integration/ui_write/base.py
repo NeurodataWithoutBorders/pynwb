75bddfdae5f8050f1740e65288fbc38174d3d1b2
Ryan Ly <rly@lbl.gov>
2020-01-15 15:27:25 -0800

    Update base.py

--- tests/integration/ui_write/base.py
+++ tests/integration/ui_write/base.py
@@ -9,7 +9,7 @@ import warnings
 
 from pynwb import get_manager, NWBFile, NWBHDF5IO, validate as pynwb_validate
 from pynwb.testing import remove_test_file
-from hdmf.backends import BrokenLinkWarning
+from hdmf.backends.warnings import BrokenLinkWarning
 from hdmf.backends.hdf5 import HDF5IO
 from hdmf.build.warnings import MissingRequiredWarning, OrphanContainerWarning
 from hdmf.container import Data, Container
