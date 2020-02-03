46a4486c9f0de97690dac6c382baf8837c5a82cd
Ryan Ly <rly@lbl.gov>
2020-02-03 09:39:26 -0800

    Fix and refactor retinotopy.py to match spec, add tests

--- src/pynwb/retinotopy.py
+++ src/pynwb/retinotopy.py
@@ -1,63 +1,93 @@
 from collections.abc import Iterable
 
-from hdmf.utils import docval, popargs, call_docval_func
+from hdmf.utils import docval, popargs, call_docval_func, get_docval
 
 from . import register_class, CORE_NAMESPACE
-from .core import NWBContainer, NWBDataInterface
+from .core import NWBDataInterface, NWBData
+from .image import GrayscaleImage
 
 
-class AImage(NWBContainer):
+@register_class('RetinotopyImage', CORE_NAMESPACE)
+class RetinotopyImage(GrayscaleImage):
+    """Gray-scale image related to retinotopic mapping. Array structure: [num_rows][num_columns].
     """
-    """
 
-    __nwbfields__ = ('data',
-                     'bits_per_pixel',
+    __nwbfields__ = ('bits_per_pixel',
                      'dimension',
                      'format',
-                     'field_of_view',
-                     'focal_depth')
+                     'field_of_view')
 
-    @docval({'name': 'name', 'type': str, 'doc': 'the name of this axis map'},
-            {'name': 'data', 'type': Iterable, 'doc': 'Data field.'},
+    @docval({'name': 'name', 'type': str, 'doc': 'Name of the retinotopy image'},
+            {'name': 'data', 'type': Iterable, 'shape': (None, None),
+             'doc': 'Image data. Array structure: [num_rows][num_columns]'},
             {'name': 'bits_per_pixel', 'type': int,
              'doc': 'Number of bits used to represent each value. This is necessary to determine maximum '
                     '(white) pixel value.'},
-            {'name': 'dimension', 'type': Iterable, 'doc': 'Number of rows and columns in the image.'},
-            {'name': 'format', 'type': Iterable, 'doc': 'Format of image. Right now only "raw" supported.'},
-            {'name': 'field_of_view', 'type': Iterable, 'doc': 'Size of viewing area, in meters.'},
+            {'name': 'dimension', 'type': Iterable, 'shape': (2, ),
+             'doc': 'Number of rows and columns in the image as a 2-element Iterable. NOTE: row, column representation '
+                    'is equivalent to height, width.'},
+            {'name': 'format', 'type': Iterable, 'doc': 'Format of image. Right now only "raw" is supported.'},
+            {'name': 'field_of_view', 'type': Iterable, 'shape': (2, ),
+             'doc': 'Size of viewing area, in meters, as a 2-element Iterable'})
+    def __init__(self, **kwargs):
+        bits_per_pixel, dimension, format, field_of_view = popargs(
+            'bits_per_pixel', 'dimension', 'format', 'field_of_view', kwargs)
+        call_docval_func(super().__init__, kwargs)
+        self.bits_per_pixel = bits_per_pixel
+        self.dimension = dimension
+        self.format = format
+        self.field_of_view = field_of_view
+
+
+class FocalDepthImage(RetinotopyImage):
+
+    __nwbfields__ = ('focal_depth',)
+
+    @docval(*get_docval(RetinotopyImage.__init__),
             {'name': 'focal_depth', 'type': 'float', 'doc': 'Focal depth offset, in meters.'})
     def __init__(self, **kwargs):
-        data, bits_per_pixel, dimension, format, field_of_view = popargs(
-            'data', 'bits_per_pixel', 'dimension', 'format', 'field_of_view', kwargs)
-        call_docval_func(super(AImage, self).__init__, kwargs)
-        self.data = data
-        self.bits_per_pixel = bits_per_pixel
-        self.dimension = format
-        self.field_of_view = field_of_view
+        focal_depth = popargs('focal_depth', kwargs)
+        call_docval_func(super().__init__, kwargs)
+        self.focal_depth = focal_depth
 
 
-class AxisMap(NWBContainer):
-    """
-    """
+AImage = RetinotopyImage  # alias to maintain backward compatibility
 
-    __nwbfields__ = ('data',
-                     'field_of_view',
-                     'unit',
+
+@register_class('RetinotopyMap', CORE_NAMESPACE)
+class RetinotopyMap(NWBData):
+    """Abstract two-dimensional map of responses"""
+
+    __nwbfields__ = ('field_of_view',
                      'dimension')
 
-    @docval({'name': 'name', 'type': str, 'doc': 'the name of this axis map'},
-            {'name': 'data', 'type': Iterable, 'shape': (None, None), 'doc': 'data field.'},
-            {'name': 'field_of_view', 'type': Iterable, 'doc': 'Size of viewing area, in meters.'},
-            {'name': 'unit', 'type': str, 'doc': 'Unit that axis data is stored in (e.g., degrees)'},
-            {'name': 'dimension', 'type': Iterable, 'shape': (None, ),
+    @docval({'name': 'name', 'type': str, 'doc': 'Name of this axis map'},
+            {'name': 'data', 'type': Iterable, 'shape': (None, None),
+             'doc': 'Image data. Array structure: [num_rows][num_columns]'},
+            {'name': 'field_of_view', 'type': Iterable, 'shape': (2, ),
+             'doc': 'Size of viewing area, in meters.'},
+            {'name': 'dimension', 'type': Iterable, 'shape': (2, ),
              'doc': 'Number of rows and columns in the image'})
     def __init__(self, **kwargs):
-        data, field_of_view, unit, dimension = popargs('data', 'field_of_view', 'unit', 'dimension', kwargs)
-        call_docval_func(super(AxisMap, self).__init__, kwargs)
-        self.data = data
+        field_of_view, dimension = popargs('field_of_view', 'dimension', kwargs)
+        call_docval_func(super().__init__, kwargs)
         self.field_of_view = field_of_view
-        self.unit = unit
         self.dimension = dimension
+
+
+@register_class('AxisMap', CORE_NAMESPACE)
+class AxisMap(RetinotopyMap):
+    """Abstract two-dimensional map of responses to stimuli along a single response axis (e.g. eccentricity)"""
+
+    __nwbfields__ = ('unit', )
+
+    @docval(*get_docval(RetinotopyMap.__init__, 'name', 'data', 'field_of_view'),
+            {'name': 'unit', 'type': str, 'doc': 'Unit that axis data is stored in (e.g., degrees)'},
+            *get_docval(RetinotopyMap.__init__, 'dimension'))
+    def __init__(self, **kwargs):
+        unit = popargs('unit', kwargs)
+        call_docval_func(super().__init__, kwargs)
+        self.unit = unit
 
 
 @register_class('ImagingRetinotopy', CORE_NAMESPACE)
@@ -80,7 +110,7 @@ class ImagingRetinotopy(NWBDataInterface):
                      'focal_depth_image',
                      'vasculature_image',)
 
-    @docval({'name': 'sign_map', 'type': AxisMap,
+    @docval({'name': 'sign_map', 'type': RetinotopyMap,
              'doc': 'Sine of the angle between the direction of the gradient in axis_1 and axis_2.'},
             {'name': 'axis_1_phase_map', 'type': AxisMap,
              'doc': 'Phase response to stimulus on the first measured axis.'},
@@ -92,13 +122,13 @@ class ImagingRetinotopy(NWBDataInterface):
             {'name': 'axis_2_power_map', 'type': AxisMap,
              'doc': 'Power response on the second measured axis. Response is scaled so 0.0 is no '
                      'power in the response and 1.0 is maximum relative power.'},
-            {'name': 'axis_descriptions', 'type': Iterable,
+            {'name': 'axis_descriptions', 'type': Iterable, 'shape': (2, ),
              'doc': 'Two-element array describing the contents of the two response axis fields. '
                     'Description should be something like ["altitude", "azimuth"] or ["radius", "theta"].'},
-            {'name': 'focal_depth_image', 'type': AImage,
+            {'name': 'focal_depth_image', 'type': FocalDepthImage,
              'doc': 'Gray-scale image taken with same settings/parameters (e.g., focal depth, wavelength) '
                     'as data collection. Array format: [rows][columns].'},
-            {'name': 'vasculature_image', 'type': AImage,
+            {'name': 'vasculature_image', 'type': RetinotopyImage,
              'doc': 'Gray-scale anatomical image of cortical surface. Array structure: [rows][columns].'},
             {'name': 'name', 'type': str, 'doc': 'the name of this container', 'default': 'ImagingRetinotopy'})
     def __init__(self, **kwargs):
@@ -106,7 +136,7 @@ class ImagingRetinotopy(NWBDataInterface):
             focal_depth_image, sign_map, vasculature_image = popargs(
                 'axis_1_phase_map', 'axis_1_power_map', 'axis_2_phase_map', 'axis_2_power_map',
                 'axis_descriptions', 'focal_depth_image', 'sign_map', 'vasculature_image', kwargs)
-        call_docval_func(super(ImagingRetinotopy, self).__init__, kwargs)
+        call_docval_func(super().__init__, kwargs)
         self.axis_1_phase_map = axis_1_phase_map
         self.axis_1_power_map = axis_1_power_map
         self.axis_2_phase_map = axis_2_phase_map
