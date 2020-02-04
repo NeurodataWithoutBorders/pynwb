7611a5f30e00384e0cd8aa2715c7b2fd2cab9da2
Ryan Ly <rly@lbl.gov>
2020-02-04 12:37:06 -0800

    Fix Subject is not a DynamicTable and should not be copied as such

--- src/pynwb/file.py
+++ src/pynwb/file.py
@@ -667,7 +667,7 @@ class NWBFile(MultiContainerInterface):
         # HDF5 object references cannot point to objects external to the file. Both DynamicTables such as TimeIntervals
         # contain such object references and types such as ElectricalSeries contain references to DynamicTables.
         # Below, copy the table and link to the columns so that object references work.
-        fields_to_copy = ['electrodes', 'epochs', 'trials', 'units', 'subject', 'sweep_table', 'invalid_times']
+        fields_to_copy = ['electrodes', 'epochs', 'trials', 'units', 'sweep_table', 'invalid_times']
         for field in fields_to_copy:
             if field in kwargs:
                 if isinstance(self.fields[field], DynamicTable):
