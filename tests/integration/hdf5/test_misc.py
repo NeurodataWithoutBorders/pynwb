4728b8485c0c39d4a533878a607b3d1a14593b01
Ryan Ly <rly@lbl.gov>
2020-02-20 11:18:38 -0800

    Fix test -- floats should be passed in for waveform_mean/sd

--- tests/integration/hdf5/test_misc.py
+++ tests/integration/hdf5/test_misc.py
@@ -13,9 +13,9 @@ class TestUnitsIO(AcquisitionH5IOMixin, TestCase):
         """ Return the test Units to read/write """
         ut = Units(name='UnitsTest', description='a simple table for testing Units')
         ut.add_unit(spike_times=[0, 1, 2], obs_intervals=[[0, 1], [2, 3]],
-                    waveform_mean=[1, 2, 3], waveform_sd=[4, 5, 6])
+                    waveform_mean=[1., 2., 3.], waveform_sd=[4., 5., 6.])
         ut.add_unit(spike_times=[3, 4, 5], obs_intervals=[[2, 5], [6, 7]],
-                    waveform_mean=[1, 2, 3], waveform_sd=[4, 5, 6])
+                    waveform_mean=[1., 2., 3.], waveform_sd=[4., 5., 6.])
         ut.waveform_rate = 40000.
         ut.resolution = 1/40000
         return ut
