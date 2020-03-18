b69b6aa7eee369c00c97a5777af59c6cbc7136fc
Ryan Ly <rly@lbl.gov>
2020-03-18 10:57:44 -0700

    Fix bugged device constructor

--- tests/unit/test_device.py
+++ tests/unit/test_device.py
@@ -2,9 +2,13 @@ from pynwb.device import Device
 from pynwb.testing import TestCase
 
 
-class DeviceConstructorBoth(TestCase):
+class TestDevice(TestCase):
 
     def test_init(self):
-        device = Device(name='device_name')
+        device = Device(name='device_name',
+                        description='description',
+                        manufacturer='manufacturer')
 
         self.assertEqual(device.name, 'device_name')
+        self.assertEqual(device.description, 'description')
+        self.assertEqual(device.manufacturer, 'manufacturer')
