64878dd06a62deaaf3c1a2a9536459048d53af7b
Ryan Ly <rly@lbl.gov>
2021-05-24 17:39:17 -0700

    Fix setup.py reqs

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ print('found these packages:', pkgs)
 schema_dir = 'nwb-schema/core'
 
 reqs = [
-    'h5py==2.9,<3',
-    'hdmf==2.5.6,<3',
-    'numpy==1.16,<1.21',
-    'pandas==1.0.5,<2',
-    'python-dateutil==2.7,<3',
+    'h5py>=2.9,<3',
+    'hdmf>=2.5.6,<3',
+    'numpy>=1.16,<1.21',
+    'pandas>=1.0.5,<2',
+    'python-dateutil>=2.7,<3',
     'setuptools'
 ]
 
