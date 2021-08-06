284b6893c3886c0dd4c59467ac947aeb5616c240
Ryan Ly <rly@lbl.gov>
2021-08-06 10:53:12 -0700

    Remove ExternalImageSeriesMap

--- src/pynwb/io/image.py
+++ src/pynwb/io/image.py
@@ -11,18 +11,3 @@ class ImageSeriesMap(TimeSeriesMap):
         super().__init__(spec)
         external_file_spec = self.spec.get_dataset('external_file')
         self.map_spec('starting_frame', external_file_spec.get_attribute('starting_frame'))
-
-
-@register_map(ExternalImageSeries)
-class ExternalImageSeriesMap(NWBContainerMapper):
-
-    def __init__(self, spec):
-        super().__init__(spec)
-
-        timestamps_spec = self.spec.get_dataset('timestamps')
-        self.map_spec('timestamps_unit', timestamps_spec.get_attribute('unit'))
-        self.map_spec('interval', timestamps_spec.get_attribute('interval'))
-
-        startingtime_spec = self.spec.get_dataset('starting_time')
-        self.map_spec('starting_time_unit', startingtime_spec.get_attribute('unit'))
-        self.map_spec('rate', startingtime_spec.get_attribute('rate'))
