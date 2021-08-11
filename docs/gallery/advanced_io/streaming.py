444f9390016e81b01a9c2aba9e493892c6e7fd5a
Ryan Ly <rly@lbl.gov>
2021-08-11 16:30:21 -0700

    Update tutorial

--- docs/gallery/advanced_io/streaming.py
+++ docs/gallery/advanced_io/streaming.py
@@ -22,51 +22,14 @@ from a large NWB file stored remotely.
 #
 
 ####################
-# Next, in Python, import the ``requests`` package and define helper functions for accessing data
-# from the DANDI Archive.
+# Next, use the :py:func:`~pynwb.utils.get_dandi_s3_url` function to get the S3 URL to an NWB file of interest
+# stored in the DANDI Archive.
 #
 # .. code-block:: python
 #
-#   import requests
+#   from pynwb.utils import get_dandi_s3_url
 #
-#   def _search_dandi_assets(url, filepath):
-#       response = requests.request("GET", url, headers={"Accept": "application/json"}).json()
-#
-#       for asset in response["results"]:
-#           if filepath == asset["path"]:
-#               return asset["asset_id"]
-#
-#       if response.get("next", None):
-#           return _search_dandi_assets(response["next"], filepath)
-#
-#       return None
-#
-#
-#   def get_dandi_asset_id(dandiset_id, filepath):
-#       url = f"https://api.dandiarchive.org/api/dandisets/{dandiset_id}/versions/draft/assets/"
-#       asset_id = _search_dandi_assets(url, filepath)
-#       if asset_id is None:
-#           raise ValueError(f'path {filepath} not found in dandiset {dandiset_id}.')
-#       return asset_id
-#
-#
-#   def get_dandi_s3_url(dandiset_id, filepath):
-#       """Get the s3 location for any NWB file on DANDI"""
-#
-#       asset_id = get_dandi_asset_id(dandiset_id, filepath)
-#       url = f"https://api.dandiarchive.org/api/dandisets/{dandiset_id}/versions/draft/assets/{asset_id}/download/"
-#
-#       s3_url = requests.request(url=url, method='head').url
-#       if '?' in s3_url:
-#           return s3_url[:s3_url.index('?')]
-#       return s3_url
-
-####################
-# Next, use the helper functions above to get the S3 URL to an NWB file of interest stored in the DANDI Archive.
-#
-# .. code-block:: python
-#
-#   dandiset_id = '000006'  # ephys dataset, Svoboda Lab
+#   dandiset_id = '000006'  # ephys dataset from the Svoboda Lab
 #   filepath = 'sub-anm372795/sub-anm372795_ses-20170718.nwb'  # 450 kB file
 #   s3_path = get_dandi_s3_url(dandiset_id, filepath)
 
