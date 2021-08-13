8d79cad711af0f04bd2546bd89fbbc4c8607d0d8
Ryan Ly <rly@lbl.gov>
2021-08-12 22:09:53 -0700

    Change ros3 tests to remove namespace warning

--- tests/integration/ros3/test_ros3.py
+++ tests/integration/ros3/test_ros3.py
@@ -8,7 +8,7 @@ class TestRos3Streaming(TestCase):
     def test_read(self):
         s3_path = 'https://dandiarchive.s3.amazonaws.com/ros3test.nwb'
 
-        with NWBHDF5IO(s3_path, mode='r', load_namespaces=True, driver='ros3') as io:
+        with NWBHDF5IO(s3_path, mode='r', driver='ros3') as io:
             nwbfile = io.read()
             test_data = nwbfile.acquisition['ts_name'].data[:]
             self.assertEqual(len(test_data), 3)
@@ -17,7 +17,7 @@ class TestRos3Streaming(TestCase):
         # this is the NWB Test Data dandiset #000126 sub-1/sub-1.nwb
         s3_path = 'https://dandiarchive.s3.amazonaws.com/blobs/11e/c89/11ec8933-1456-4942-922b-94e5878bb991'
 
-        with NWBHDF5IO(s3_path, mode='r', load_namespaces=True, driver='ros3') as io:
+        with NWBHDF5IO(s3_path, mode='r', driver='ros3') as io:
             nwbfile = io.read()
             test_data = nwbfile.acquisition['TestData'].data[:]
             self.assertEqual(len(test_data), 3)
