2282111a6fc1e3bfaf3b51f09e97cf79b1695765
bendichter <ben.dichter@gmail.com>
2022-08-30 17:50:03 -0400

    add mock subject to mock nwbfile

--- src/pynwb/testing/mock/file.py
+++ src/pynwb/testing/mock/file.py
@@ -9,12 +9,14 @@ def mock_NWBFile(
     session_description='session_description',
     identifier=None,
     session_start_time=datetime(1970, 1, 1),
+    subject=None,
     **kwargs
 ):
     return NWBFile(
         session_description=session_description,
         identifier=identifier or uuid4(),
         session_start_time=session_start_time,
+        subject=subject or mock_Subject(),
         **kwargs
     )
 
