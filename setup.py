c20ab280ea44e43046db62b7f0570c0f03853428
mavaylon1 <mavaylon1@berkeley.edu>
2022-10-19 16:25:52 -0700

    first

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup_args = {
     'packages': pkgs,
     'package_dir': {'': 'src'},
     'package_data': {'pynwb': ["%s/*.yaml" % schema_dir, "%s/*.json" % schema_dir]},
+    'python_requires': '>=3.7',
     'classifiers': [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
