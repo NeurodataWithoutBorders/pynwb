4f8f7ebda947bf2207cf336dfc83230057f25f21
Ryan Ly <rly@lbl.gov>
2022-10-19 09:17:20 -0700

    Fix warnings (#1571)
    * Fix warnings
    
    * Make sure unittest CI runs test_mock
    
    * Update changelog
    
    * Cut number of tests run on CI

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,6 +14,8 @@
 ### Bug fixes
 - Fixed bug in ``pynwb.testing.mock.file.mock_NWBFile`` to identifier UUID to string. @oruebel
   [#1557](https://github.com/NeurodataWithoutBorders/pynwb/pull/1557)
+- Minor fixes to test suite to prevent warnings. @rly
+  [#1571](https://github.com/NeurodataWithoutBorders/pynwb/pull/1571)
 
 ## PyNWB 2.1.1 (September 1, 2022)
 
--- tests/unit/test_mock.py
+++ tests/unit/test_mock.py
@@ -29,30 +29,33 @@ from pynwb.testing.mock.ecephys import (
     mock_SpikeEventSeries,
 )
 
+from pynwb.testing import TestCase
 import pytest
 
 
-@pytest.mark.parametrize(
-    "mock_function", [
-        mock_ImagingPlane,
-        mock_TwoPhotonSeries,
-        mock_RoiResponseSeries,
-        mock_PlaneSegmentation,
-        mock_OpticalChannel,
-        mock_OptogeneticStimulusSite,
-        mock_OptogeneticSeries,
-        mock_Device,
-        mock_Position,
-        mock_PupilTracking,
-        mock_CompassDirection,
-        mock_SpatialSeries,
-        mock_ElectrodeGroup,
-        mock_ElectrodeTable,
-        mock_ElectricalSeries,
-        mock_SpikeEventSeries,
-        mock_Subject,
-        mock_NWBFile,
-    ],
-)
-def test_mock(mock_function):
-    mock_function()
+class TestMock(TestCase):
+
+    @pytest.mark.parametrize(
+        "mock_function", [
+            mock_ImagingPlane,
+            mock_TwoPhotonSeries,
+            mock_RoiResponseSeries,
+            mock_PlaneSegmentation,
+            mock_OpticalChannel,
+            mock_OptogeneticStimulusSite,
+            mock_OptogeneticSeries,
+            mock_Device,
+            mock_Position,
+            mock_PupilTracking,
+            mock_CompassDirection,
+            mock_SpatialSeries,
+            mock_ElectrodeGroup,
+            mock_ElectrodeTable,
+            mock_ElectricalSeries,
+            mock_SpikeEventSeries,
+            mock_Subject,
+            mock_NWBFile,
+        ],
+    )
+    def test_mock(mock_function):
+        mock_function()
