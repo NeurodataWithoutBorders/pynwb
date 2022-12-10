5bf000cd276e7a585dfea0070be70bf65de5c472
Oliver Ruebel <oruebel@lbl.gov>
2022-12-10 01:45:16 -0800

    Fix bug in duecredit Bibtex

--- src/pynwb/__init__.py
+++ src/pynwb/__init__.py
@@ -313,7 +313,7 @@ article_type = {journal},
 title = {{The Neurodata Without Borders ecosystem for neurophysiological data science}},
 author = {R\"ubel, Oliver and Tritt, Andrew and Ly, Ryan and Dichter, Benjamin K and Ghosh, Satrajit and Niu, Lawrence and Baker, Pamela and Soltesz, Ivan and Ng, Lydia and Svoboda, Karel and Frank, Loren and Bouchard, Kristofer E},
 editor = {Colgin, Laura L and Jadhav, Shantanu P},
-volume = {11{,
+volume = {11},
 year = {2022},
 month = {oct},
 pub_date = {2022-10-04},
@@ -324,8 +324,7 @@ url = {https://doi.org/10.7554/eLife.78362},
 keywords = {Neurophysiology, data ecosystem, data language, data standard, FAIR data, archive},
 journal = {eLife},
 issn = {2050-084X},
-publisher = {eLife Sciences Publications, Ltd},
-}
+publisher = {eLife Sciences Publications, Ltd}}
 """), description="The Neurodata Without Borders ecosystem for neurophysiological data science",  # noqa: E501
          path="pynwb/", version=__version__, cite_module=True)
 del due, BibTeX
