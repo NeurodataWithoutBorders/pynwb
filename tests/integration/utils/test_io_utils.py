e4d11862d5ffcd3103d64cd4f0c1a9cd772616c2
rly <rly@lbl.gov>
2022-12-13 21:05:09 -0800

    Run IO utils tests in test suite

---
+++ tests/integration/utils/test_io_utils.py
@@ -0,0 +1,30 @@
+"""Tests related to pynwb.io.utils."""
+import pytest
+
+from hdmf.build import GroupBuilder
+from pynwb.io.utils import get_nwb_version
+from pynwb.testing import TestCase
+
+
+class TestGetNWBVersion(TestCase):
+
+    def test_get_nwb_version(self):
+        """Get the NWB version from a builder."""
+        builder1 = GroupBuilder(name="root")
+        builder1.set_attribute(name="nwb_version", value="2.0.0")
+        builder2 = GroupBuilder(name="another")
+        builder1.set_group(builder2)
+        assert get_nwb_version(builder1) == (2, 0, 0)
+        assert get_nwb_version(builder2) == (2, 0, 0)
+
+    def test_get_nwb_version_missing(self):
+        """Get the NWB version from a builder where the root builder does not have an nwb_version attribute."""
+        builder1 = GroupBuilder(name="root")
+        builder2 = GroupBuilder(name="another")
+        builder1.set_group(builder2)
+
+        with pytest.raises(ValueError, match="'nwb_version' attribute is missing from the root of the NWB file."):
+            get_nwb_version(builder1)
+
+        with pytest.raises(ValueError, match="'nwb_version' attribute is missing from the root of the NWB file."):
+            get_nwb_version(builder1)
