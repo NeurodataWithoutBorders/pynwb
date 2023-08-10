3b611a1703f46be402c91c42232d61b1e5d45d46
mavaylon1 <mavaylon1@berkeley.edu>
2023-08-09 17:01:55 -0700

    update

--- src/pynwb/resources.py
+++ src/pynwb/resources.py
@@ -1,9 +1,9 @@
-from hdmf.common import ExternalResources as hdmf_ExternalResources
+from hdmf.common import HERD as hdmf_ExternalResources
 from . import get_type_map as tm
 from hdmf.utils import docval, get_docval
 
 
-class ExternalResources(hdmf_ExternalResources):
+class HERD(hdmf_ExternalResources):
     @docval(*get_docval(hdmf_ExternalResources.__init__))
     def __init__(self, **kwargs):
         kwargs['type_map'] = tm()
