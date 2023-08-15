977a3bc8e8e0ff256ff605690a0f29315ecbfdb8
Ben Dichter <ben.dichter@gmail.com>
2023-08-15 18:20:09 -0400

    improve the read tutorial (#1757)
    * improve the read tutorial
    
    * flake8
    
    * add download
    
    * fix filename
    
    * have read tutorial link out to NWB Overview and rename with plot_ prefix
    
    * update doc requirements to include dandi
    
    * flake8
    
    * move streaming just after download
    
    * remove filler output
    
    * more fixes to read tutorial
    
    * remove old image files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -146,6 +146,8 @@ intersphinx_mapping = {
     'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
     'dandi': ('https://dandi.readthedocs.io/en/stable/', None),
     'fsspec': ("https://filesystem-spec.readthedocs.io/en/latest/", None),
+    'nwbwidgets': ("https://nwb-widgets.readthedocs.io/en/latest/", None),
+    'nwb-overview': ("https://nwb-overview.readthedocs.io/en/latest/", None),
 }
 
 extlinks = {
