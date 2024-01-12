15290289f6304a3a83c6fcf8bf4110c15dc1683c
Ryan Ly <rly@lbl.gov>
2024-01-11 16:38:29 -0800

    Disable nightly dandi read workflow for now (#1794)

--- tests/read_dandi/read_first_nwb_asset.py
+++ tests/read_dandi/read_first_nwb_asset.py
@@ -1,5 +1,7 @@
-"""Test reading NWB files from the DANDI Archive using ROS3."""
+"""Test reading NWB files from the DANDI Archive using fsspec."""
 from dandi.dandiapi import DandiAPIClient
+import fsspec
+import h5py
 import random
 import sys
 import traceback
@@ -10,15 +12,20 @@ from pynwb import NWBHDF5IO
 # NOTE: do not name the function with "test_" prefix, otherwise pytest
 # will try to run it as a test
 
+# TODO read dandisets systematically, not randomly
+# see https://github.com/NeurodataWithoutBorders/pynwb/issues/1804
+
 def read_first_nwb_asset():
-    """Test reading the first NWB asset from a random selection of 50 dandisets that uses NWB."""
-    num_dandisets_to_read = 50
+    """Test reading the first NWB asset from a random selection of 2 dandisets that uses NWB."""
+    num_dandisets_to_read = 2
     client = DandiAPIClient()
     dandisets = list(client.get_dandisets())
     random.shuffle(dandisets)
     dandisets_to_read = dandisets[:num_dandisets_to_read]
     print("Reading NWB files from the following dandisets:")
     print([d.get_raw_metadata()["identifier"] for d in dandisets_to_read])
+
+    fs = fsspec.filesystem("http")
 
     failed_reads = dict()
     for i, dandiset in enumerate(dandisets_to_read):
@@ -47,8 +54,10 @@ def read_first_nwb_asset():
         s3_url = first_asset.get_content_url(follow_redirects=1, strip_query=True)
 
         try:
-            with NWBHDF5IO(path=s3_url, driver="ros3") as io:
-                io.read()
+            with fs.open(s3_url, "rb") as f:
+                with h5py.File(f) as file:
+                    with NWBHDF5IO(file=file) as io:
+                        io.read()
         except Exception as e:
             print(traceback.format_exc())
             failed_reads[dandiset] = e
