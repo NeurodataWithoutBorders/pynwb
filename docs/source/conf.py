2788c09ce72f6a1fcc74fb48b18b2164b4da2af9
bendichter <ben.dichter@gmail.com>
2024-01-25 13:26:03 -0500

    Add Zarr IO tutorial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -148,6 +148,7 @@ intersphinx_mapping = {
     'fsspec': ("https://filesystem-spec.readthedocs.io/en/latest/", None),
     'nwbwidgets': ("https://nwb-widgets.readthedocs.io/en/latest/", None),
     'nwb-overview': ("https://nwb-overview.readthedocs.io/en/latest/", None),
+    'hdmf-zarr': ("https://hdmf-zarr.readthedocs.io/en/latest/", None),
 }
 
 extlinks = {
@@ -159,6 +160,7 @@ extlinks = {
     'hdmf-docs': ('https://hdmf.readthedocs.io/en/stable/%s', '%s'),
     'dandi': ('https://www.dandiarchive.org/%s', '%s'),
     "nwbinspector": ("https://nwbinspector.readthedocs.io/en/dev/%s", "%s"),
+    'hdmf-zarr': ('https://hdmf-zarr.readthedocs.io/en/latest/%s', '%s'),
 }
 
 # Add any paths that contain templates here, relative to this directory.
