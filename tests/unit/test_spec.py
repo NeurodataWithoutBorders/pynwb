f696a79befd0b3296be29c4082c3d385aa668d04
Ryan Ly <rly@lbl.gov>
2024-07-03 23:11:46 -0700

    FIx test

--- tests/unit/test_spec.py
+++ tests/unit/test_spec.py
@@ -46,9 +46,10 @@ class NWBDatasetSpecTests(TestCase):
             assert not hasattr(obj, "value")
 
         dataset_spec_dict = dict(name="warnme", dtype="float", value=0.0, doc="doc")
-        msg = "Unexpected keys ['value'] in spec {'name': 'warnme', 'dtype': 'float32, 'value': 0.0, 'doc': 'doc.'}"
         ret = NWBDatasetSpec.build_const_args(dataset_spec_dict)
         assert ret == dataset_spec_dict
+        
+        msg = "Unexpected keys ['value'] in spec {'name': 'warnme', 'dtype': 'float', 'value': 0.0, 'doc': 'doc.'}"
         with self.assertWarnsWith(UserWarning, msg):
             obj = NWBDatasetSpec.build_spec(dataset_spec_dict)
             assert obj.name == "warnme"
