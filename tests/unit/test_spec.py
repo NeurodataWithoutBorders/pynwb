8880ee6246b0fc3c08b23b28d9d7f5dbc84d3c4b
Ryan Ly <rly@lbl.gov>
2024-07-03 22:57:28 -0700

    Fix typo

--- tests/unit/test_spec.py
+++ tests/unit/test_spec.py
@@ -44,5 +44,5 @@ class NWBDatasetSpecTests(TestCase):
 
         dataset_spec_dict = dict(name="warnme", dtype="float", value=0.0, doc="doc")
         msg = "Unexpected keys ['value'] in spec {'name': 'warnme', 'dtype': 'float32, 'value': 0.0, 'doc': 'doc.'}"
-        with self.assertWarnsWith(UserWarning, msg)
+        with self.assertWarnsWith(UserWarning, msg):
             obj = NWBDatasetSpec.build_const_args(dataset_spec_dict)
