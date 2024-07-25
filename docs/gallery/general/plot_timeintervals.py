ac8e20327086ccca229cd3ca72a940c73ec4de61
Ryan Ly <rly@lbl.gov>
2024-07-25 15:18:41 -0700

    Update plot_timeintervals.py

--- docs/gallery/general/plot_timeintervals.py
+++ docs/gallery/general/plot_timeintervals.py
@@ -93,7 +93,7 @@ nwbfile.add_acquisition(rate_ts)
 # for the column and a description of what the column stores. You do not need to supply data
 # type, as this will inferred. Once all columns have been added, trial data can be populated using
 # :py:meth:`~pynwb.file.NWBFile.add_trial`. Note that if you add a custom column, you must
-# have add at least one row to write the table to a file.
+# add at least one row to write the table to a file.
 #
 # Lets add an additional column and some trial data with tags and timeseries references.
 
