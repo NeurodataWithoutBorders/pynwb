a143e14e8328d01b3fbb0bd304a03d1b8d22ec12
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-11-09 13:25:08 -0600

    try pytest import skip

--- tests/integration/hdf5/test_io.py
+++ tests/integration/hdf5/test_io.py
@@ -615,9 +615,9 @@ class TestNWBHDF5IO(TestCase):
 
         read_nwbfile.get_read_io().close()
     
-    @pytest.mark.skipif(not pytest.importorskip("fsspec"), reason="fsspec library not available")
     def test_read_nwb_method_s3_path(self):
-        
+        fsspec = pytest.importorskip("fsspec")  # This alone will skip if import fails
+
         s3_test_path = "https://dandiarchive.s3.amazonaws.com/blobs/11e/c89/11ec8933-1456-4942-922b-94e5878bb991"
         read_nwbfile = NWBHDF5IO.read_nwb(path=s3_test_path)
         assert read_nwbfile.identifier == "3f77c586-6139-4777-a05d-f603e90b1330"
