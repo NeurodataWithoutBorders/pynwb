c2cf78bb9d2442358eda07a70642ab26348fcd0c
rly <rly@lbl.gov>
2024-11-18 16:14:15 -0800

    Add optional model_number, model_name, serial_number to Device

--- tests/unit/test_device.py
+++ tests/unit/test_device.py
@@ -5,10 +5,18 @@ from pynwb.testing import TestCase
 class TestDevice(TestCase):
 
     def test_init(self):
-        device = Device(name='device_name',
-                        description='description',
-                        manufacturer='manufacturer')
+        device = Device(
+            name='device_name',
+            description='description',
+            manufacturer='manufacturer',
+            model_number='model_number',
+            model_name='model_name',
+            serial_number='serial_number',
+        )
 
         self.assertEqual(device.name, 'device_name')
         self.assertEqual(device.description, 'description')
         self.assertEqual(device.manufacturer, 'manufacturer')
+        self.assertEqual(device.model_number, 'model_number')
+        self.assertEqual(device.model_name, 'model_name')
+        self.assertEqual(device.serial_number, 'serial_number')
