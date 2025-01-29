b5a8f15c69fd6850858384ef947de90f496d1169
Ben Dichter <ben.dichter@gmail.com>
2025-01-29 11:40:15 -0500

    Update src/pynwb/testing/mock/file.py
    Co-authored-by: Ryan Ly <rly@lbl.gov>

--- src/pynwb/testing/mock/file.py
+++ src/pynwb/testing/mock/file.py
@@ -10,7 +10,7 @@ from .utils import name_generator
 def mock_NWBFile(
     session_description: str = 'session_description',
     identifier: Optional[str] = None,
-    session_start_time: datetime = datetime(1970, 1, 1, 12, tzinfo=tzutc()),
+    session_start_time: datetime = datetime(1970, 1, 1, tzinfo=tzutc()),
     **kwargs
 ):
     return NWBFile(
