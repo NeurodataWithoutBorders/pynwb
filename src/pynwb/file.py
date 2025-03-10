eac70677c427f57b8a4f6a32c51ba76dc61b1f5e
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-03-10 16:00:43 -0700

    update to pass on construct

--- src/pynwb/file.py
+++ src/pynwb/file.py
@@ -491,7 +491,7 @@ class NWBFile(MultiContainerInterface, HERDManager):
         if timestamps_reference_time is None:
             args_to_set['timestamps_reference_time'] = args_to_set['session_start_time']
         elif timestamps_reference_time.tzinfo is None:
-            self._error_on_new_warn_on_construct(
+            self._error_on_new_pass_on_construct(
                     error_msg="'timestamps_reference_time' must be a timezone-aware datetime object."
                 )
 
