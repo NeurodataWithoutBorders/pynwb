74dfd85b6b6708798cf51635581b31731894376a
rly <rly@lbl.gov>
2025-07-13 15:52:35 -0700

    Skip streaming test if offline

--- tests/integration/hdf5/test_io.py
+++ tests/integration/hdf5/test_io.py
@@ -21,7 +21,7 @@ from pynwb.testing.mock.file import mock_NWBFile
 import unittest
 try:
     import fsspec # noqa f401
-    HAVE_FSSPEC = True 
+    HAVE_FSSPEC = True
 except ImportError:
     HAVE_FSSPEC = False
 
@@ -595,36 +595,38 @@ class TestNWBHDF5IO(TestCase):
         self.assertFalse(NWBHDF5IO.can_read(__file__))
 
     def test_read_nwb_method_path(self):
-        
+
         # write the example file
         with NWBHDF5IO(self.path, 'w') as io:
             io.write(self.nwbfile)
-            
+
         # test that the read_nwb method works
         read_nwbfile = NWBHDF5IO.read_nwb(path=self.path)
         self.assertContainerEqual(read_nwbfile, self.nwbfile)
 
         read_nwbfile.get_read_io().close()
-        
+
     def test_read_nwb_method_file(self):
-        
+
         # write the example file
         with NWBHDF5IO(self.path, 'w') as io:
             io.write(self.nwbfile)
-            
+
         import h5py
 
         file = h5py.File(self.path, 'r')
-        
+
         read_nwbfile = NWBHDF5IO.read_nwb(file=file)
         self.assertContainerEqual(read_nwbfile, self.nwbfile)
 
         read_nwbfile.get_read_io().close()
-    
+
     @unittest.skipIf(not HAVE_FSSPEC, "fsspec library not available")
     def test_read_nwb_method_s3_path(self):
         s3_test_path = "https://dandiarchive.s3.amazonaws.com/blobs/11e/c89/11ec8933-1456-4942-922b-94e5878bb991"
-        read_nwbfile = NWBHDF5IO.read_nwb(path=s3_test_path)
-        assert read_nwbfile.identifier == "3f77c586-6139-4777-a05d-f603e90b1330"
-    
-        assert read_nwbfile.subject.subject_id == "1"
+        try:
+            read_nwbfile = NWBHDF5IO.read_nwb(path=s3_test_path)
+            assert read_nwbfile.identifier == "3f77c586-6139-4777-a05d-f603e90b1330"
+            assert read_nwbfile.subject.subject_id == "1"
+        except FileNotFoundError:
+            self.skipTest("Internet access to DANDI failed. Skipping test.")
