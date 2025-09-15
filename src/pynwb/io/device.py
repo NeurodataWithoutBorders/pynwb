f68197ec9d88819da08e26aea593b5ca4dcc672f
Steph Prince <40640337+stephprince@users.noreply.github.com>
2025-09-15 15:16:52 -0700

    update device mapping warnings for clarity

--- src/pynwb/io/device.py
+++ src/pynwb/io/device.py
@@ -1,4 +1,3 @@
-from hdmf.build import LinkBuilder
 from warnings import warn
 
 from .. import register_map
@@ -29,7 +28,7 @@ class DeviceMapper(NWBContainerMapper):
         """
         model_builder = builder.get('model')
         if isinstance(model_builder, str):
-            warn(f'Device.model was detected as a string. Remapping {model_builder} to a DeviceModel',
+            warn(f'Device.model was detected as a string. Remapping "{model_builder}" to a DeviceModel',
                  stacklevel=3)
 
             # replace the model string with a DeviceModel object using the model name and device attributes 
@@ -56,8 +55,9 @@ class DeviceMapper(NWBContainerMapper):
 
             # add the conflicting Device.model object as a new attribute on Device
             # e.g. Device.model in the file -> Device.ndx_optogenetics_model in the python object
-            warn('Device.model was detected as a non-DeviceModel object. '
-                 f'Adding it as a new attribute at Device.{model.namespace.replace("-", "_")}_model',
+            warn(f'The model attribute of the Device "{device_obj.name}" was detected as a non-DeviceModel '
+                 f'object. Data associated with this object can be accessed at '
+                 f'"nwbfile.devices["{device_obj.name}"].{model.namespace.replace("-", "_")}_model"',
                  stacklevel=2)
             setattr(device_obj, f"{model.namespace.replace('-', '_')}_model", model)
 
